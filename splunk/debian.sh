d241e941139d0fcb3cce3ae7533d83b5520de762
David Stirn <davidmstirn@gmail.com>
2021-11-22 19:20:13 -0500

    WIP: created debian script

---
+++ splunk/debian.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://download.splunk.com/products/universalforwarder/releases/8.2.3/linux/splunkforwarder-8.2.3-cd0848707637-linux-2.6-amd64.deb
+sudo apt install ./splunkforwarder-8.2.3-cd0848707637-linux-2.6-amd64.deb
