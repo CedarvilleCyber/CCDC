461cbc778188343a0ec86aaee2c81e63325aab8c
David Stirn <davidmstirn@gmail.com>
2022-02-25 17:50:11 -0500

    Updated Forwarder to match changes to server setup

--- splunk/setup_forwarder.sh
+++ splunk/setup_forwarder.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+read -p "Is forwarder installed [y/n]? " installed
+
+if [[ "$installed" = "n" ]] #installed
+then
+
 if [[ `id -u` -ne 0 ]]
 then
 	echo "Requires super user privileges"
-	#exit 1
+	exit 1
 fi
 
 #detect os flavor to know which files to monitor by default
@@ -18,38 +23,84 @@ then
 	exit 1
 fi
 
-#Setup
+else
+
+#===============================#
+#-------------Setup-------------#
+#===============================#
 export SPLUNK_HOME=/opt/splunkforwarder
-#TODO Add default monitors
+
+machines=("Debian-DNS-NTP"
+	  "Ubuntu-Web"
+	  "Ubuntu-Wkst"
+	  "Splunk"
+	  "CentOS-E-comm"
+	  "Fedora-Webmail-WebApps")
+
+select machine in "${machines[@]}"
+do
+	case $machine in
+		"Debian-DNS-NTP")
+			cat <<-EOF > $SPLUNK_HOME/etc/system/local/inputs.conf
+			[monitor:///var/log/syslog]
+			index=syslog
+			sourcetype=syslog
+
+			[monitor:///var/log/auth.log]
+			index=auth
+			sourcetype=linux-secure
+			EOF
+			break;;
+	esac
+done
+
+cp $SPLUNK_HOME/etc/system/default/inputs.conf $SPLUNK_HOME/etc/system/local
+cat << EOF > $SPLUNK_HOME/etc/system/local/inputs.conf
+[monitor:///var/log/syslog]
+index=syslog
+sourcetype=syslog
+
+[monitor:///var/log/auth.log]
+index=auth
+sourcetype=linux-secure
+EOF
 
 #=================================#
 #----------------SSL--------------#
 #=================================#
 mkdir $SPLUNK_HOME/etc/auth/mycerts
-read -p "Enter Splunk Server IP: " serverip
-echo "Downloading certificates from splunk server..."
-scp -T root@$serverip:"~/quarantine/mycerts/myCombinedServerCertificate.pem ~/quarantine/mycerts/myCACertificate.pem" $SPLUNK_HOME/etc/auth/mycerts
+certfile=$machine.pem
+echo "$certfile"
 
-read -p "Enter SSL password: " sslpwd
+read -p "Enter Splunk Server IP: " serverip
+read -p "Enter username: " splunkuname
+echo "Downloading certificates from splunk server..."
+ssh -l $splunkuname $serverip "sudo cp /opt/splunk/etc/auth/mycerts/$certfile /tmp; sudo chmod go+r /tmp/$certfile"
+scp $splunkuname@$serverip:"/tmp/$certfile" "$SPLUNK_HOME/etc/auth/mycerts"
+ssh -l $splunkuname $serverip "sudo cp /opt/splunk/etc/auth/mycerts/cacert.pem /tmp; sudo chmod go+r /tmp/$certfile"
+scp $splunkuname@$serverip:"/tmp/cacert.pem" "$SPLUNK_HOME/etc/auth/mycerts"
+
+read -sp "Enter SSL password: " sslpwd
 
 cat << EOF > $SPLUNK_HOME/etc/system/local/outputs.conf
-[tcpout:data]
-server=$serverip:9997
+[tcpout:splunkssl]
+server = $serverip:9997
+compressed = true
 disabled = 0
-useSSL = true
-clientCert = $SPLUNK_HOME/etc/auth/mycerts/myCombinedServerCertificate.pem
+clientCert = $SPLUNK_HOME/etc/auth/mycerts/$certfile
 useClientSSLCompression = true
 sslPassword = $sslpwd
-sslCommonNameToCheck = David Stirn
+sslCommonNameToCheck = Cedarville
 sslVerifyServerCert = true 
 EOF
 
 cat << EOF > $SPLUNK_HOME/etc/system/local/server.conf
 [sslConfig]
-sslRootCAPath = $SPLUNK_HOME/etc/auth/mycerts/myCACertificate.pem
+sslRootCAPath = $SPLUNK_HOME/etc/auth/mycerts/cacert.pem
 EOF
 
-
 #Start
-$SPLUNK_HOME/bin/splunk start --accept-license
+$SPLUNK_HOME/bin/splunk restart --accept-license
 $SPLUNK_HOME/bin/splunk enable boot-start
+
+fi #installed
