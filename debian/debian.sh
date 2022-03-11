27805e5f4ee9924c9c05acb3fb7bf992579c98f0
kaichye <74321555+kaichye@users.noreply.github.com>
2022-03-11 10:17:24 -0500

    Add files via upload

---
+++ debian/debian.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+
+clear
+
+
+# formatting text
+# Use colors, but only if connected to a terminal
+# and that terminal supports colors
+if which tput >/dev/null 2>&1; then
+	ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+	info=$(tput setaf 2)
+	error=$(tput setaf 1)
+	warn=$(tput setaf 3)
+	reset=$(tput sgr0)
+else
+	info=""
+	error=""
+	warn=""
+	reset=""
+fi
+
+
+# Debian Script
+
+if [ "$(id -u)" != "0" ]; then
+	printf "You must be a superuser!\n"
+	printf "Meaning you are currently not super enough!\n"
+	exit 1
+fi
+
+# Give user one more chance before running script
+
+printf "You are "
+whoami
+printf "\n"
+printf "Your current working directory is "
+pwd
+printf "Continue running script? [y/n]: "
+read input
+
+if [ $input == "N" ] || [ $input == "n" ]; then
+	printf "Script Ended.\n"
+	exit 0
+fi
+
+
+# update
+printf "Updating Packages...\n"
+
+# update updates list of availible packages and versions
+apt-get update -y
+# upgrade actually install the newer versions
+apt-get upgrade -y
+# autoremove removes packages that are no longer required
+apt-get autoremove -y
+# autoclean removes the downloaded packages becuase they are already installed
+apt-get autoclean -y
+
+printf "Packages Updated!\n"
+
+
+# basic security
+printf "Removing unnecessary packages...\n"
+
+# purge deletes configuration files with it
+systemctl disable telnet
+apt-get purge telnet -y
+apt-get purge nc -y
+apt-get purge john -y
+
+printf "Stopping unnecessary services...\n"
+systemctl disable sshd
+systemctl disable ssh.service
+systemctl disable rpcbind
+systemctl disable nfs-server
+systemctl disable dovecot
+systemctl disable slapd
+systemctl disable vsftpd
+systemctl disable snmpd
+systemctl disable exim4
+systemctl disable snmp
+systemctl disable pop3
+systemctl disable icmp
+systemctl disable sendmail
+systemctl stop sshd
+systemctl stop ssh.service
+systemctl stop rpcbind
+systemctl stop nfs-server
+systemctl stop dovecot
+systemctl stop slapd
+systemctl stop vsftpd
+systemctl stop snmpd
+systemctl stop exim4
+systemctl stop snmp
+systemctl stop pop3
+systemctl stop icmp
+systemctl stop sendmail
+printf "Use (service <service name> status) to show status\n"
+
+printf "Removed!\n"
+
+# turn bind9 on though
+systemctl start bind9
+
+
+printf "Scanners and Configuring Firewall...\n"
+apt-get install clamav clamav-daemon -y
+freshclam
+apt-get install lynis -y
+apt-get install fail2ban -y
+apt-get install ufw -y
+ufw allow dns
+ufw allow ntp
+ufw enable
+
+printf "Make sure there are no werid rules in iptables and ufw!\n"
+printf "Do that manually!\n"
+
+# Install other stuff
+apt-get install libpam-pwquality -y
+apt-get install libpam-tmpdir -y
+apt-get install debian-goodies -y
+
+
+# Set up tmux
+printf "Setting up tmux...\n"
+apt-get install tmux -y
+
+# name session Background
+# Have programs running in the background
+SESSIONB="Background"
+SESSIONEXISTS=$(tmux ls | grep $SESSIONB)
+
+# Check if session already exists
+if [ "$SESSIONEXISTS" == "" ]; then
+	# doesn't already exist
+	tmux new-session -d -s $SESSIONB
+	
+	# First window (already created)
+	tmux rename-window -t 0 "Bash"
+	
+	# Second window for Clamav
+	tmux new-window -t $SESSIONB:1 -n "ClamAv"
+	# Send text to "ClamAv" window
+	# C-m means <enter>
+	tmux send-keys -t "ClamAv" "clamscan -i -r / -l clamav.log" C-m
+
+	# Third window for lynis
+	# lynis will search for vulnerabilities what you should to to fix them
+	tmux new-window -t $SESSIONB:2 -n "Lynis"
+	tmux send-keys -t "Lynis" "lynis audit system --quick > lynis.log" C-m
+	
+	# Attach session
+	#tmux attach-session -t $SESSIONB
+else
+	printf "${warn}Session \"$SESSIONB\" already exists!${reset}\n"
+fi
+
+# name session Work
+SESSIONW="Work"
+SESSIONEXISTS=$(tmux ls | grep $SESSIONW)
+
+# Check if session already exists
+if [ "$SESSIONEXISTS" == "" ]; then
+	# doesn't already exist
+	tmux new-session -d -s $SESSIONW
+	
+	# First window (already created)
+	tmux rename-window -t 0 "Bash"
+	# write command to check ntp
+	tmux send-keys -t "Bash" "./ntpRestart.sh"
+	
+	# Second window for ntp
+	apt-get install ntp -y
+	tmux new-window -t $SESSIONW:1 -n "NTPConfig"
+	tmux send-keys -t "NTPConfig" "vi /etc/ntp.conf" C-m
+
+	# Third window for Clamav
+	tmux new-window -t $SESSIONW:2 -n "ClamAvParse"
+	# Sleep to wait for the shell to load
+	sleep 0.1
+	tmux send-keys -t "ClamAvParse" "### Make sure to wait for clamav to finish! ###" C-m
+	tmux send-keys -t "ClamAvParse" "vi clamav.log"
+
+	# Forth window for lynis
+	tmux new-window -t $SESSIONW:3 -n "LynisParse"
+	#sleep 0.1
+	tmux send-keys -t "LynisParse" "### Make sure to wait for lynis to finish! ###" C-m
+	tmux send-keys -t "LynisParse" "grep suggestion /var/log/lynis-report.dat | less"
+	
+	# Fifth window for checking users
+	tmux new-window -t $SESSIONW:4 -n "CheckUsers"
+	tmux send-keys -t "CheckUsers" "./userSort.sh | less" C-m
+
+	# Sixth window for checking services
+	tmux new-window -t $SESSIONW:5 -n "CheckServices"
+	tmux send-keys -t "CheckServices" "./serviceSort.sh | less" C-m
+
+	# Seventh window for checking crontab, ufw, and iptables
+	tmux new-window -t $SESSIONW:6 -n "CheckCron+Wall"
+
+	# Attach session
+	#tmux attach-session -t $SESSIONW
+else
+	printf "${warn}Session \"$SESSIONW\" already exists!${reset}\n"
+fi
+
+# New Session Monitor
+SESSIONM="Mon"
+SESSIONEXISTS=$(tmux ls | grep $SESSIONM)
+
+if [ "$SESSIONEXISTS" == "" ]; then
+	tmux new-session -d -s $SESSIONM
+	
+	tmux rename-window -t 0 "Bash"
+
+	tmux new-window -t $SESSIONM:1 -n "Top"
+	tmux send-keys -t "Top" "top" C-m
+
+	tmux new-window -t $SESSIONM:2 -n "Syslog+SSH"
+	tmux send-keys -t "Syslog+SSH" "tail -f /var/log/syslog" C-m
+	# splits the window vertically
+	# one half on top and one half on bottom
+	tmux split-window -v
+	tmux send-keys -t "Syslog+SSH" "tail -f /var/log/auth.log | grep sshd" C-m
+	
+	# Attach Session
+	#tmux attach-session -t $SESSIONM
+else
+	printf "${warn}Session \"$SESSIONM\" already exists!${reset}\n"
+fi
+
+
+# Write login banner
+
+# Before Entering a Password
+printf "\nOnly authorized users are welcome on this system. If you are not authorized, do not continue. Make sure you have proper reason and ability to access this system.\n\nAll network activity is being logged and monitored. These logs can be used to investigate and prosecute unauthorized access.\n\n" > /etc/issue.net
+
+# After Entering a Password
+printf "\nOnly authorized users are welcome on this system. If you are not authorized, do not continue. Make sure you have proper reason and ability to access this system.\n\nAll network activity is being logged and monitored. These logs can be used to investigate and prosecute unauthorized access.\n\n" > /etc/motd
+
+
+printf "${info}Done!${reset}\n\n"
+
+exit 0
+
