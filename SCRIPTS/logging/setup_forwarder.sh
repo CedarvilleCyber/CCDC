1443686e007ca8bef30e64c1a5cc60771b79f86e
David Stirn <davidmstirn@gmail.com>
2022-10-18 15:54:25 -0400

    Updates to docs and client setup script

--- SCRIPTS/logging/setup_forwarder.sh
+++ SCRIPTS/logging/setup_forwarder.sh
@@ -232,14 +232,10 @@ echo "$certfile"
 
 read -p "Enter Splunk Server IP: " serverip
 read -p "Enter username: " splunkuname
+myusername=`whoami`
+read -p "Enter your IP address: " myip
 echo "Downloading certificates from splunk server..."
-ssh -l $splunkuname $serverip "sudo cp /opt/splunk/etc/auth/mycerts/$certfile /tmp; sudo chmod go+r /tmp/$certfile"
-scp $splunkuname@$serverip:"/tmp/$certfile" "$SPLUNK_HOME/etc/auth/mycerts"
-ssh -l $splunkuname $serverip "sudo rm -rf /tmp/$certfile"
-
-ssh -l $splunkuname $serverip "sudo cp /opt/splunk/etc/auth/mycerts/cacert.pem /tmp; sudo chmod go+r /tmp/cacert.pem"
-scp $splunkuname@$serverip:"/tmp/cacert.pem" "$SPLUNK_HOME/etc/auth/mycerts"
-ssh -l $splunkuname $serverip "sudo rm -rf /tmp/cacert.pem"
+ssh -l $splunkuname $serverip "sudo cp /opt/splunk/etc/auth/mycerts/$certfile /tmp; sudo chmod go+r /tmp/$certfile; scp $myusername@$myip:'$SPLUNK_HOME/etc/auth/mycerts' '/tmp/$certfile'; sudo rm -rf /tmp/$certfile; sudo cp /opt/splunk/etc/auth/mycerts/cacert.pem /tmp; sudo chmod go+r /tmp/cacert.pem; scp $myusername@$myip:'$SPLUNK_HOME/etc/auth/mycerts' '/tmp/cacert.pem'; sudo rm -rf /tmp/cacert.pem"
 
 getpasswd()
 {
