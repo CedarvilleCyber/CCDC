771d68d769256ff7a6a50b6765c725c35a177023
David Stirn <davidmstirn@gmail.com>
2022-10-18 02:10:02 -0400

    Major fixes to splunk ssl configuration and script flow. SSL should now work and the scripts are more organized.

--- SCRIPTS/logging/setup_server.sh
+++ SCRIPTS/logging/setup_server.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
 
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "This script automatically configures a preinstalled"
+   echo "Splunk Enterprise instance. This includes SSL"
+   echo "configuration and certificate generation for all"
+   echo "CCDC machines."
+   echo
+   echo "Syntax: setup_server [-h]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "C     Path to file containing CA Passphrase"
+   echo "S     Path to file containing Server Passphrase"
+   echo
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+# Set variables
+CAPass="stdin"
+ServerPass="stdin"
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":hC:S:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      C) # Read CA Passphrase
+         CAPassFile=$OPTARG
+         CAPass=file:$OPTARG;;
+      S) # Read Server Passphrase
+         ServerPassFile=$OPTARG
+         ServerPass=file:$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
 if [[ `id -u` -ne 0 ]]
 then
 	echo "Please run as root"
 	exit 1
 fi
-
-su - splunk
 
 #Setup
 export SPLUNK_HOME=/opt/splunk
@@ -23,36 +71,30 @@ $SPLUNK_HOME/bin/splunk add index web
 #=================================#
 mkdir $SPLUNK_HOME/etc/auth/mycerts
 export OPENSSL_CONF=$SPLUNK_HOME/openssl/openssl.cnf
-
-getsslpasswd()
-{
-	local sslpwd
-	local sslpwd2
-	read -sp "Enter SSL password: " sslpwd
-	echo ""
-	read -sp "Verify SSL password: " sslpwd2
-	echo ""
-	while [ "$sslpwd" != "$sslpwd2" ]
-	do
-		echo "Passwords do not match, try again"
-		return getpasswd
-	done
-	return $sslpwd
-}
+CASUBJ="/C=US/ST=Ohio/L=Cedarville/O=Cedarville University/OU=Cyber Team/CN=cedarville"
 
 #Create root certificate
 echo "Creating Root CA"
-$SPLUNK_HOME/bin/genRootCA.sh -pd $SPLUNK_HOME/etc/auth/mycerts
+#$SPLUNK_HOME/bin/genRootCA.sh -pd $SPLUNK_HOME/etc/auth/mycerts
+$SPLUNK_HOME/bin/splunk cmd openssl genrsa -aes256 -out $SPLUNK_HOME/etc/auth/mycerts/ca.key -passout "$CAPass" 2048
+$SPLUNK_HOME/bin/splunk cmd openssl req -new -key $SPLUNK_HOME/etc/auth/mycerts/ca.key -out $SPLUNK_HOME/etc/auth/mycerts/ca.csr -subj "$CASUBJ" -passin "$CAPass"
+$SPLUNK_HOME/bin/splunk cmd openssl x509 -req -in $SPLUNK_HOME/etc/auth/mycerts/ca.csr -sha512 -signkey $SPLUNK_HOME/etc/auth/mycerts/ca.key -CAcreateserial -out $SPLUNK_HOME/etc/auth/mycerts/cacert.pem -days 1095 -passin "$CAPass"
 
 #Append appsCA.pem to Root CA
 cat $SPLUNK_HOME/etc/auth/appsCA.pem >> $SPLUNK_HOME/etc/auth/mycerts/cacert.pem
 
 #Create indexer certificate
 indexername="Indexer"
+IDXSUBJ="/C=US/ST=Ohio/L=Cedarville/O=Cedarville University/OU=Cyber Team/CN=indexer.cedarville"
 echo "Creating Indexer Cert"
-$SPLUNK_HOME/bin/splunk createssl server-cert -d $SPLUNK_HOME/etc/auth/mycerts -n $indexername -c indexer.cedarville
+#$SPLUNK_HOME/bin/splunk createssl server-cert -d $SPLUNK_HOME/etc/auth/mycerts -n $indexername -c indexer.cedarville
+$SPLUNK_HOME/bin/splunk cmd openssl genrsa -aes256 -out $SPLUNK_HOME/etc/auth/mycerts/$indexername.key -passout "$ServerPass" 2048
+$SPLUNK_HOME/bin/splunk cmd openssl req -new -key $SPLUNK_HOME/etc/auth/mycerts/$indexername.key -out $SPLUNK_HOME/etc/auth/mycerts/$indexername.csr -subj "$IDXSUBJ" -passin "$ServerPass"
+$SPLUNK_HOME/bin/splunk cmd openssl x509 -req -in $SPLUNK_HOME/etc/auth/mycerts/$indexername.csr -SHA256 -CA $SPLUNK_HOME/etc/auth/mycerts/cacert.pem -CAkey $SPLUNK_HOME/etc/auth/mycerts/ca.key -CAcreateserial -out $SPLUNK_HOME/etc/auth/mycerts/${indexername}Cert.pem -days 1095 -passin "$CAPass"
+cat $SPLUNK_HOME/etc/auth/mycerts/${indexername}Cert.pem $SPLUNK_HOME/etc/auth/mycerts/$indexername.key $SPLUNK_HOME/etc/auth/mycerts/cacert.pem > $SPLUNK_HOME/etc/auth/mycerts/$indexername.pem
 
 #Create forwarder certificates
+FWDSUBJ="/C=US/ST=Ohio/L=Cedarville/O=Cedarville University/OU=Cyber Team/CN="
 echo "Creating Forwarder Certs"
 machines=("Docker-Remote"
 	  "Debian-DNS-NTP"
@@ -64,27 +106,28 @@ machines=("Docker-Remote"
 	  "Fedora-Webmail-WebApps")
 commonnames=""
 
-sslpwd=getsslpasswd
 for m in "${machines[@]}"
 do
 	echo "Creating Cert for: $m"
-	$SPLUNK_HOME/bin/splunk createssl server-cert -d $SPLUNK_HOME/etc/auth/mycerts -n "$m" -c $m.cedarville
-	commonnames="$commonnames $m.cedarville, "
+	#$SPLUNK_HOME/bin/splunk createssl server-cert -d $SPLUNK_HOME/etc/auth/mycerts -n "$m" -c $m.cedarville
+	$SPLUNK_HOME/bin/splunk cmd openssl genrsa -aes256 -out $SPLUNK_HOME/etc/auth/mycerts/$m.key -passout "$ServerPass" 2048
+	$SPLUNK_HOME/bin/splunk cmd openssl req -new -key $SPLUNK_HOME/etc/auth/mycerts/$m.key -out $SPLUNK_HOME/etc/auth/mycerts/$m.csr -subj "$FWDSUBJ$m.cedarville" -passin "$ServerPass"
+	$SPLUNK_HOME/bin/splunk cmd openssl x509 -req -in $SPLUNK_HOME/etc/auth/mycerts/$m.csr -SHA256 -CA $SPLUNK_HOME/etc/auth/mycerts/cacert.pem -CAkey $SPLUNK_HOME/etc/auth/mycerts/ca.key -CAcreateserial -out $SPLUNK_HOME/etc/auth/mycerts/${m}Cert.pem -days 1095 -passin "$CAPass"
+	cat $SPLUNK_HOME/etc/auth/mycerts/${m}Cert.pem $SPLUNK_HOME/etc/auth/mycerts/$m.key $SPLUNK_HOME/etc/auth/mycerts/cacert.pem > $SPLUNK_HOME/etc/auth/mycerts/$m.pem
+	commonnames="$commonnames$m.cedarville,"
 done
-commonnames=$(commonnames%,*)
+commonnames=${commonnames::-1}
 
 #Server conf
-
-sslpwd=getsslpasswd
 cat << EOF > $SPLUNK_HOME/etc/system/local/inputs.conf
 [splunktcp-ssl:9997]
 disabled=0
 
 [SSL]
 serverCert = $SPLUNK_HOME/etc/auth/mycerts/$indexername.pem
-sslPassword = $sslpwd
+sslPassword = `cat $ServerPassFile`
 requireClientCert = true
-sslVersions *,-ssl2
+sslVersions = *,-ssl2
 sslCommonNameToCheck = $commonnames
 EOF
 
@@ -94,5 +137,6 @@ sslRootCAPath = $SPLUNK_HOME/etc/auth/mycerts/cacert.pem
 EOF
 
 #Start
+chown -R splunk:splunk /opt/splunk
 sudo $SPLUNK_HOME/bin/splunk restart --accept-license
 sudo $SPLUNK_HOME/bin/splunk enable boot-start
