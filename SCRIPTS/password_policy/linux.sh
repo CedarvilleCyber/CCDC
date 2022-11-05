37ef42c352e8068d339e43b47a2e328a27c789a6
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2022-11-05 10:25:27 -0400

    Update linux.sh

--- SCRIPTS/password_policy/linux.sh
+++ SCRIPTS/password_policy/linux.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+clear
+
+echo
+echo "---------------- 2023 CU CCDC Password Policy Guide ----------------"
+echo
+
 echo "This script gives the steps to implementing the password policy."
 echo
 echo "Configuration is different based on the distribution family used"
@@ -8,6 +14,7 @@ echo "Enter [RHEL] if you are on Ecomm, Splunk, or Webmail"
 read -p "Distribution family: " DIST_FAM
 
 echo
+echo "Follow these steps to enforce the password policy."
 echo
 case "$DIST_FAM" in
     "Debian")
@@ -28,6 +35,9 @@ case "$DIST_FAM" in
         echo "      'PASS_MAX_DAYS 180'"
         echo "      'PASS_MIN_DAYS 0'"
         echo "      'PASS_WARN_AGE 7'"
+        echo
+        echo "    And a little further down:"
+        echo "      'LOGIN_TIMEOUT 600'"
         ;;
         
     "RHEL")
@@ -39,11 +49,23 @@ case "$DIST_FAM" in
         # Requires at least one of each lower, upper, digit, and symbol 
         echo "    Add 'minclass=4 minlen=15' to line with:"
         echo "      'password   requisite   pam_pwquality.so'"
+        echo
+        # Set password expiration
+        echo "# vi /etc/login.defs"
+        echo "    Find and set:"
+        echo "      'PASS_MAX_DAYS 180'"
+        echo "      'PASS_MIN_DAYS 0'"
+        echo "      'PASS_WARN_AGE 7'"
+        echo
+        echo "    And add the line:"
+        echo "      'LOGIN_TIMEOUT 600'"
         ;;
     
     *)
         echo "Re-run script and enter 'Debian' or 'RHEL' exactly"
         exit 1
 esac
+
+clear
 
 exit 0
