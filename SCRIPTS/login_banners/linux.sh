dbca3835974adf8005664d428ecb386fdeadba48
arrxnov <70920682+arrxnov@users.noreply.github.com>
2022-11-11 16:23:20 -0500

    Update for CentOS/RHEL ONLY

--- SCRIPTS/login_banners/linux.sh
+++ SCRIPTS/login_banners/linux.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+if (which yum)
+then
+  echo "###############################" > /etc/motd
+  echo "## This system may not be    ##" >> /etc/motd
+  echo "## accessed without proper   ##" >> /etc/motd
+  echo "## authorization. There is   ##" >> /etc/motd
+  echo "## no reasonable expectation ##" >> /etc/motd
+  echo "## of privacy. Thank you.    ##" >> /etc/motd
+  echo "###############################" >> /etc/motd
+fi
+  
+if (which apt-get)
+then
+  # Do y'all's thing
+fi
