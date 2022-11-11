8ae522365e510af70411152379f1000843ef7ba0
smiddy007 <70818821+smiddy007@users.noreply.github.com>
2022-11-11 18:16:09 -0500

    Update linux.sh

--- SCRIPTS/login_banners/linux.sh
+++ SCRIPTS/login_banners/linux.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo "###############################" > /etc/motd
-echo "## This system may not be    ##" >> /etc/motd
-echo "## accessed without proper   ##" >> /etc/motd
-echo "## authorization. There is   ##" >> /etc/motd
-echo "## no reasonable expectation ##" >> /etc/motd
-echo "## of privacy. Thank you.    ##" >> /etc/motd
-echo "###############################" >> /etc/motd
+# script to setup login banners
+BANNER="Warning: Only authorized users are permitted to login. All network activity is being monitored and logged, and may be used to investigate and prosecute any instance of unauthorized access."
+echo $BANNER | sudo tee -a /etc/issue /etc/issue.net > /dev/null
+echo $BANNER | sudo tee /etc/ssh/sshd-banner > /dev/null
+echo "Banner /etc/ssh/sshd-banner" | sudo tee -a /etc/ssh/sshd_config > /dev/null
