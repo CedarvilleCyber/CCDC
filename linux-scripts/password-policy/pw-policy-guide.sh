254f650325fb7796f7e1e859d82c7cbe22d23184
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2022-12-05 18:47:58 -0500

    Update and rename pw-policy.sh to pw-policy-guide.sh

--- linux-scripts/password-policy/pw-policy-guide.sh
+++ linux-scripts/password-policy/pw-policy-guide.sh
@@ -15,13 +15,15 @@ echo "Follow these steps to enforce the password policy"
 echo
 echo
 case "$ID" in
-    "debian"|"ubuntu")        
+    "debian"|"ubuntu")   
         echo "Open /etc/pam.d/common-password in a text editor"
+        
         # Prevent re-use of past 3 passwords
         echo "    Add 'remember=3' to line starting with:"
         echo "      'password   [success=1 default=ignore]  pam_unix.so'"
         echo "    and change sha512 to sha256
         echo
+        
         # Requires at least one of each lower, upper, digit, and symbol 
         echo "    Add this line above the previous line:"
         echo "      'password   required    pam_pwquality.so minclass=4 minlen=15'"
@@ -29,9 +31,10 @@ case "$ID" in
         echo "    The two lines should look like this:"
         echo "      'password   required    pam_pwquality.so minclass=4 minlen=15'"
         echo "      'password   [success=1 default=ignore]  pam_unix.so obscure \"
-        echo "           use_auth_tok first_try_pass sha256 minlen=15'"
+        echo "           use_auth_tok first_try_pass sha256'"
         echo
         echo
+        
         # Set password expiration
         echo "Open /etc/login.defs in a text editor"
         echo "    Find and set:"
@@ -44,18 +47,14 @@ case "$ID" in
         ;;
         
     "fedora"|"centos")
-    
-    ##########################################################################
-                                    ATTENTION
-    The Splunk vm system_auth file has changed, may need to make modifications
-    ##########################################################################
-    
         echo "Open /etc/pam.d/system-auth in a text editor"
+        
         # Prevent re-use of past 3 passwords
         echo "    Add 'remember=3' to line starting with:"
         echo "      'password   sufficient  pam_unix.so'"
         echo "    and change sha512 to sha256 and remove nollok"
         echo
+        
         # Requires at least one of each lower, upper, digit, and symbol 
         echo "    Add 'minclass=4 minlen=15' to line with:"
         echo "      'password   requisite   pam_pwquality.so'"
@@ -68,6 +67,7 @@ case "$ID" in
         echo "          try_first_pass use_authtok remember=3'"
         echo
         echo
+        
         # Set password expiration
         echo "Open /etc/login.defs in a text editor"
         echo "    Find and set:"
