6ad2f3c57871407fa02107c305523a1768d9e25d
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2022-12-05 16:35:22 -0500

    Update auto-pw-policy.sh

--- linux-scripts/password-policy/auto-pw-policy.sh
+++ linux-scripts/password-policy/auto-pw-policy.sh
@@ -2,16 +2,26 @@
 
 clear
 
+# File contains the line "ID = [distribution]" and calling `source` makes it a variable 
 source /etc/os-release
 
-# Set the distribution-dependent config file
+# Backs-up original configuration files to pw-policy-config folder, appends _OLD to name
+# Overwrites configuration file with pre-written one in pw-policy-config folder
+
+# Distribution-dependent config file
 case $ID in
     "debian"|"ubuntu")
+        mv /etc/pam.d/common-password ./pw-policy-config/common-password_OLD
+        mv ./pw-policy-config/debian-common-password /etc/pam.d/common-password
         ;;
     "fedora"|"centos")
+        mv /etc/pam.d/system-auth ./pw-policy-config/system-auth_OLD
+        mv ./pw-policy-config/fedora-system-auth /etc/pam.d/system-auth
         ;;
 esac
 
-# Set the distribution-independent config file
+# Distribution-independent config file
+mv /etc/login.defs ./pw-policy-config/login.defs_OLD
+mv ./pw-policy-config/login.defs /etc/login.defs
 
 exit 0
