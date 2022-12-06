66bdfc5fe61af786a2a390dd2af4c7b17078766c
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2022-12-06 15:43:47 -0500

    Update auto-pw-policy.sh

--- linux-scripts/password-policy/auto-pw-policy.sh
+++ linux-scripts/password-policy/auto-pw-policy.sh
@@ -11,17 +11,17 @@ source /etc/os-release
 # Distribution-dependent config file
 case $ID in
     "debian"|"ubuntu")
-        mv /etc/pam.d/common-password ./pw-policy-config/common-password_OLD
-        mv ./pw-policy-config/debian-common-password /etc/pam.d/common-password
+        cp /etc/pam.d/common-password ./pw-policy-config/common-password_OLD
+        cp ./pw-policy-config/debian-common-password /etc/pam.d/common-password
         ;;
     "fedora"|"centos")
-        mv /etc/pam.d/system-auth ./pw-policy-config/system-auth_OLD
-        mv ./pw-policy-config/fedora-system-auth /etc/pam.d/system-auth
+        cp /etc/pam.d/system-auth ./pw-policy-config/system-auth_OLD
+        cp ./pw-policy-config/fedora-system-auth /etc/pam.d/system-auth
         ;;
 esac
 
 # Distribution-independent config file
-mv /etc/login.defs ./pw-policy-config/login.defs_OLD
-mv ./pw-policy-config/login.defs /etc/login.defs
+cp /etc/login.defs ./pw-policy-config/login.defs_OLD
+cp ./pw-policy-config/login.defs /etc/login.defs
 
 exit 0
