539f1d1ca4f5726abc5bb52ec322f5a233dc8d28
smiddy007 <70818821+smiddy007@users.noreply.github.com>
2022-12-06 16:42:25 -0500

    restart ssh

--- linux-scripts/login-banners.sh
+++ linux-scripts/login-banners.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# script to setup login banners for redhat and debian machines
+# script to setup login banners for any linux machine
 BANNER="Warning: Only authorized users are permitted to login. All network activity is being monitored and logged, and may be used to investigate and prosecute any instance of unauthorized access."
-echo $BANNER | sudo tee -a /etc/issue /etc/issue.net > /dev/null
-echo $BANNER | sudo tee /etc/ssh/sshd-banner > /dev/null
-echo "Banner /etc/ssh/sshd-banner" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+echo $BANNER | tee -a /etc/issue /etc/issue.net > /dev/null
+echo $BANNER | tee /etc/ssh/sshd-banner > /dev/null
+echo "Banner /etc/ssh/sshd-banner" | tee -a /etc/ssh/sshd_config > /dev/null
+/etc/init.d/sshd restart
