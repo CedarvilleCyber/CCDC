61492175bc32bcf4a5e349b1f3fe92f3dd9c2131
smiddy007 <70818821+smiddy007@users.noreply.github.com>
2022-12-06 16:56:28 -0500

    no

--- linux-scripts/login-banners.sh
+++ linux-scripts/login-banners.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # script to setup login banners for any linux machine
+echo "The login banner script must be run as root"
 BANNER="Warning: Only authorized users are permitted to login. All network activity is being monitored and logged, and may be used to investigate and prosecute any instance of unauthorized access."
 echo $BANNER | tee -a /etc/issue /etc/issue.net > /dev/null
 echo $BANNER | tee /etc/ssh/sshd-banner > /dev/null
 echo "Banner /etc/ssh/sshd-banner" | tee -a /etc/ssh/sshd_config > /dev/null
-/etc/init.d/sshd restart
+
+# TODO check if redhat or debian and run one or the other
+sudo /etc/init.d/sshd restart
+sudo /etc/init.d/ssh restart
