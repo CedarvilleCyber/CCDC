2f0c2e25df0619c95f2540ef2a452d172bb132e0
David Reid <94802444+dwreidjr@users.noreply.github.com>
2023-01-10 16:55:18 -0500

    Update master-script.sh

--- linux-scripts/master-script.sh
+++ linux-scripts/master-script.sh
@@ -15,25 +15,30 @@ for f in $( ls ./ ); do
 	fi
 done
 
-# Load os-release environment vars
-#	this makes ID and ID_LIKE
-#	among others available for use
-#	in this script and all scripts
-#	called by this script. However,
-#	we should set the env vars in all
-#	scripts so they are independent
-. /etc/os-release
+echo "Please enter the number of the present operating system."
+echo "0 for "
+echo "1 for "
+echo "2 for "
+echo "3 for "
+echo "4 for "
+echo "5 for "
+echo "6 for "
+echo "7 for "
+echo "8 for "
+echo "9 for "
+read $os
 
-#start executing scripts from here
-if [[ ( $ID = centos ) && ( $VERSION_ID = 7* )]]
+if [ $os = "0" ]
 then
 	
-elif [[ ( $ID = somethingelse ) ]]
+elif [ $os = "1" ]
 then
 	echo "Insert machine specific scripts here with your own elif block"
 else
 	echo "$ID does not have custom scripts"
 fi
+
+
 
 #establish log forwarder
 chmod 700 logging/install_and_setup_forwarder.sh
