f50a50f52e4e1ede0e838867c2dfb2c698dd3b5c
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2023-01-11 17:23:48 -0500

    Also checks in /var/spool/cron/crontabs

--- linux-scripts/check-cronjobs.sh
+++ linux-scripts/check-cronjobs.sh
@@ -24,20 +24,28 @@
 
 # The script currently only shows cronjobs from the crontab file and file in the cron.d directory
 # It may not be very useful to show the contents of the rest of the files and directories I found
-# If we end up testing more files/directories, it will be easier to put all of the files into an array and interate through that
 
 #!/bin/bash
 
-echo "Cronjobs are commands that are set to execute periodically. They are very helpful for sysadmins... but they can also run malicious commands (eg. once we found a reverse shell: /bin/bash)"
-echo "Check if any of the jobs (commands) listed look suspicious. Navigate to the file, screenshot for evidence and remove."
+echo "Cronjobs are commands that are set to execute periodically"
+echo "They can be useful, but frequently run malicious commands in CCDC"
+echo "Check if any of the jobs (commands) listed look suspicious"
+echo "Navigate to the file, screenshot for evidence and remove"
+echo "-----------------------------------------------------------------"
 echo
 
-echo "Cronjobs from the /etc/crontab file:"
-cat /etc/crontab | grep -E '^[0-9]|^\*' | awk '{ORS=" "; print "\t"; for (i=7; i<=NF; i++) print $i; print "\n"}'
-echo
+files=(/etc/crontab)
+directories=(/etc/cron.d /var/spool/cron/crontabs)
 
-for file in /etc/cron.d/*; do
-    echo "Cronjobs from $file:"
-    cat $file | grep -E '^[0-9]|^\*' | awk '{ORS=" "; print "\t"; for (i=7; i<=NF; i++) print $i; print "\n"}'
-    echo
+for file in ${files[@]}; do
+    echo "Cronjobs from the $file file:"
+    cat $file | grep -E '^[0-9]|^\*' | awk '{ORS=" "; print "\t"; for (i=7; i<=NF; i++) print $i; print "\n"}'                                                                echo
+done
+
+for dir in ${directories[@]}; do
+    echo "Searching in $dir directory"
+    for file in $dir/*; do
+        echo "    Cronjobs from $file:"
+        cat $file | grep -E '^[0-9]|^\*' | awk '{ORS=" "; print "\t"; for (i=7; i<=NF; i++) print $i; print "\n"}'
+        echo                                                                                                                                                                  done
 done
