7dc8af82a55fa22be241b90ff4f356e1c0fa7c30
MtSneffels <113790394+MtSneffels@users.noreply.github.com>
2023-01-26 12:58:29 -0500

    Updated to use DISTRO env var

--- linux-scripts/script-dependencies/login-banners.sh
+++ linux-scripts/script-dependencies/login-banners.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+# script to setup login banners for any linux machine
+# assumes DISTRO environment variable is present
 
 echo "Begin login-banners ..."
 
-# Put script here
+#check if user is root
+if [[ $(id -u) != "0" ]]; then
+	printf "You must be root!\n"
+	exit 1
+fi
+
+BANNER="Warning: Only authorized users are permitted to login. All network activity is being monitored and logged, and may be used to investigate and prosecute any instance of unauthorized access."
+echo $BANNER | tee -a /etc/issue /etc/issue.net > /dev/null
+echo $BANNER | tee /etc/ssh/sshd-banner > /dev/null
+echo "Banner /etc/ssh/sshd-banner" | tee -a /etc/ssh/sshd_config > /dev/null
+
+if [[ $DISTRO == "redhat" ]]; then
+    /etc/init.d/sshd restart
+else
+    /etc/init.d/ssh restart
+fi
 
 echo "... login-banners complete!"
