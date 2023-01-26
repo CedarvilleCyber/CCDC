6ea5f9c65bb61cf6056b8c2d789cf1fd46f124ea
MtSneffels <113790394+MtSneffels@users.noreply.github.com>
2023-01-26 13:04:02 -0500

    Updated to use DISTRO env var

--- linux-scripts/script-dependencies/osupdater.sh
+++ linux-scripts/script-dependencies/osupdater.sh
@@ -1,7 +1,56 @@
 #!/bin/bash
+# updates the OS and its packages
+# assumes PKG_MAN environment variable exists
 
-echo "Begin osupdater ..."
+#check if user is root
+if [[ $(id -u) != "0" ]]; then
+	printf "You must be root!\n"
+	exit 1
+fi
 
-# Put script here
+# script start
+printf "OS updater script starting\n"
+printf "Systems may stop temporarily\n"
 
-echo "... osupdater complete!"
+#apt-get
+if [[ $PKG_MAN == "apt-get" ]]; then
+
+	#updates list of available packages/versions
+	apt-get update -y
+	
+	#upgrades to the new packages/versions
+	apt-get upgrade -y
+	
+	#installing bare minimum updates
+	apt-get install unattended-upgrades -y
+
+	#removes packages that are no longer required
+	apt-get autoremove -y
+
+	#removes unneeded downloaded packages
+	apt-get autoclean -y
+
+fi
+
+
+#yum
+if [[ $PKG_MAN == "yum" ]]; then
+
+	#updates all packages
+	yum update -y
+	
+	#updates packages accounting for obsoletes
+	yum upgrade -y
+		
+	#installs available packages
+	yum install -y
+	
+	#removes uneeded packages/dependencies
+	yum autoremove -y
+	
+	#clean out all packages and meta data from cache
+	yum clean all -y
+
+fi
+
+printf "SCRIPT COMPLETE - OS & packages current.\n"
