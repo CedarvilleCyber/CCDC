62d65befe90a508380a7044a7fc79a63a8c78ac1
David Stirn <davidmstirn@gmail.com>
2023-03-01 23:33:38 -0500

    Fixed file permissions

--- linux-scripts/script-dependencies/ecomm.sh
+++ linux-scripts/script-dependencies/ecomm.sh
@@ -66,6 +66,9 @@ do
 done
 echo "Changing prestashop database password"
 cat $config_file | sed "s/\(_DB_PASSWD_', '\).*\(,);\)/\1$dbnew2\2/" > $WK_DIR/temp && mv $WK_DIR/temp $config_file
+chown apache:apache $config_file
+chmod 644 $config_file
+chcon -t httpd_sys_rw_content_t $config_file
 
 
 read -p "Update the prestashop admin password [y/n]?" update_presta_pw
