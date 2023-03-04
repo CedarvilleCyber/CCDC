7d59902637c97550fce9356d9f1fd061e394d79a
kaichye <74321555+kaichye@users.noreply.github.com>
2023-03-04 09:44:16 -0500

    Update setup-antivirus.sh

--- linux-scripts/script-dependencies/setup-antivirus.sh
+++ linux-scripts/script-dependencies/setup-antivirus.sh
@@ -19,12 +19,12 @@ $PKG_MAN install clamav clamav-daemon -y
 # Manually install database if not present
 clam_ver=$(clamscan --version | cut -d " " -f 2 | cut -d "." -f 2)
 if (("$clam_ver" < 103)); then
-    mkdir /var/lib/clamav
-    curr_dir=$( pwd )
-    cd /var/lib/clamav
-    wget http://clamavdb.c3sl.ufpr.br/main.cvd http://clamavdb.c3sl.ufpr.br/daily.cvd http://clamavdb.c3sl.ufpr.br/bytecode.cvd
-    cd curr_dir
-    freshclam
+    #mkdir /var/lib/clamav
+    #curr_dir=$( pwd )
+    #cd /var/lib/clamav
+    #wget http://clamavdb.c3sl.ufpr.br/main.cvd http://clamavdb.c3sl.ufpr.br/daily.cvd http://clamavdb.c3sl.ufpr.br/bytecode.cvd
+    #cd curr_dir
+    #freshclam
 else
     # Stop freshclam service
     systemctl stop clamav-freshclam
