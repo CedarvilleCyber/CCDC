2ff71ed438d5cfa4df03765ff798518d1f058a21
David Reid <94802444+dwreidjr@users.noreply.github.com>
2023-03-03 22:22:38 -0500

    Update master-script.sh

--- linux-scripts/script-dependencies/master-script.sh
+++ linux-scripts/script-dependencies/master-script.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 # Master script for linux
-#
-# Notes:
-# - System should be up-to-date (i.e., for debian: apt-get update)
-# - Exports 4 environment variables:
-#	- DISTRO_ID (ubuntu, centos, fedora, etc.)
-#	- DISTRO (debian | redhat)
-#	- PKG_MAN (apt-get | yum)
-#	- WK_DIR (home or working directory)
-#
-# - Additions to this script:
-#	- Please use the exported environment variables when possible
-#	- All scripts run by this one and their dependencies should be located in ./script-dependencies
-#	- machine scripts possibly need to be updated
-#	- clean-os.sh needs to be added to
+# 
+# All scripts/dependencies called should be in ./script-dependencies
+# Exports 4 environment variables:
+# - DISTRO_ID (ubuntu, centos, fedora, etc.)
+# - DISTRO (debian | redhat)
+# - PKG_MAN (apt-get | yum)
+# - WK_DIR (home or working directory)
+
+
 
 # INITIAL CHECKS
 
@@ -37,9 +32,14 @@ for f in $( ls ./script-dependencies/ ); do
     fi
 done
 
-echo "Initial checks complete. Starting script..."
+printf "\e[1;33m INITIAL CHECKS COMPLETE. STARTING LINUX MASTER SCRIPT... \e[0m \n"
+
+
 
 # IMMEDIATE SECURITY
+
+# Install systemd which provides systemctl for firewall script below
+apt-get install systemd -y
 
 # Run firewall script
 ./script-dependencies/firewall/iptables.sh
@@ -48,7 +48,7 @@ echo "Initial checks complete. Starting script..."
 
 # GET ENVIRONMENT VARIABLES
 
-# Get home or working directory from user
+# Get and export WK_DIR
 printf "Your current directory is: "
 pwd
 read -p "What is your home or primary working directory? " wk_dir
@@ -58,7 +58,7 @@ export WK_DIR=$wk_dir
 . /etc/os-release
 export DISTRO_ID=$ID
 
-# Get OS from user and export DISTRO
+# Get and export DISTRO
 read -p "Please enter your machine's distribution branch: [debian | redhat] " distro
 export DISTRO=$distro
 
@@ -88,12 +88,12 @@ chmod 750 $WK_DIR/quarantine
 # ALL PURPOSE SCRIPTS
 
 # Implement password policy
-#./script-dependencies/password-policy/pw-policy-guide.sh
+# BROKEN ./script-dependencies/password-policy/pw-policy-guide.sh
 
 # Set up login banners
 ./script-dependencies/login-banners.sh
 
-# Clean Operating System
+# Clean OS
 ./script-dependencies/clean-os.sh
 
 
@@ -101,7 +101,7 @@ chmod 750 $WK_DIR/quarantine
 # MACHINE SPECIFIC SCRIPTS
 
 # Get machine from user
-printf "Please enter the number corresponding to this machine's purpose:
+printf "Enter respective number according to machine's purpose:
     [1] Splunk Server
     [2] EComm Server
     [3] Workstation
@@ -114,13 +114,13 @@ read machine
 export MACHINE=$machine
 
 case $machine in
-    1)  ./script-dependencies/splunk-server.sh  ;;
-    2)  ./script-dependencies/ecomm.sh  ;;
-    3) 	./script-dependencies/workstation.sh  ;;
-    4) 	./script-dependencies/web-server.sh  ;;
-    5)  ./script-dependencies/dns-ntp.sh  ;;
-    6) 	./script-dependencies/webmail.sh  ;;
-    7) 	./script-dependencies/firewall.sh  ;;
+    1)  ./script-dependencies/machine-scripts/splunk-server.sh  ;;
+    2)  ./script-dependencies/machine-scripts/ecomm.sh  ;;
+    3) 	./script-dependencies/machine-scripts/workstation.sh  ;;
+    4) 	./script-dependencies/machine-scripts/web-server.sh  ;;
+    5)  ./script-dependencies/machine-scripts/dns-ntp.sh  ;;
+    6) 	./script-dependencies/machine-scripts/webmail.sh  ;;
+    7) 	./script-dependencies/machine-scripts/firewall.sh  ;;
     *)  
     	echo "Unknown machine, should have been a number between 1-7"
     	read -p "What is the name of your machine script? " script_name
@@ -131,7 +131,7 @@ esac
 
 # FINAL TASKS BEFORE TERMINATING
 
-# Splunk forwarder
+# Setup splunk forwarder
 cd ./script-dependencies/logging/
 ./install_and_setup_forwarder.sh
 cd ../..
@@ -139,7 +139,7 @@ cd ../..
 # Update OS
 ./script-dependencies/osupdater.sh
 
-# install antivirus
+# Install antivirus
 ./script-dependencies/setup-antivirus.sh
 
 # antivirus execution
@@ -155,10 +155,9 @@ fi
 # Misc installs
 $PKG_MAN install vim -y
 
-# Create and get backups folder
+# Backup /etc and /var
 mkdir $WK_DIR/backup
 chmod 750 $WK_DIR/backup
-
 cp -a /etc $WK_DIR/backup/
 cp -a /var $WK_DIR/backup/
 
@@ -169,4 +168,4 @@ for f in $( ls $WK_DIR/ ); do
     fi
 done
 
-echo "MASTER SCRIPT COMPLETE"
+printf "\e[1;32m MASTER SCRIPT COMPLETE \e[0m \n"
