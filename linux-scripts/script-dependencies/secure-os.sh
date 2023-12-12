5bdf4c2b279d35ae051f3fa3a8ef2db7aaaafb81
root <root@Debian11.debian.virtualbox.org>
2023-12-12 16:32:44 -0500

    secure-os finish

--- linux-scripts/script-dependencies/secure-os.sh
+++ linux-scripts/script-dependencies/secure-os.sh
@@ -15,7 +15,8 @@ fi
 printf "${info}Starting secure-os script${reset}\n"
 
 # change file permissions
-chmod -R 700 /home/*
+find /home ! -iname "*.sh" -type f -exec chmod 600 {} +
+find /home \( -iname "*.sh" -type f -o -type d \) -exec chmod 700 {} +
 chmod 644 /etc/passwd
 chmod 600 /etc/shadow
 chmod 600 /etc/sudoers
@@ -24,6 +25,16 @@ chmod 444 /etc/shells
 # stop php web shells
 # First find all php.ini file locations
 find / -iname "php.ini" > ./data-files/php-locations.txt 2>/dev/null
+
+# reads through each line of a file, ignoring whitespace
+while IFS="" read -r f || [ -n "$f" ]
+do
+    printf "${info}$f${reset}\n"
+    sed -i -e '/^disable_functions.*[a-zA-Z0-9]$/ s/$/,exec,shell_exec,system,passthru,popen,proc_open,pcntl_exec,pcntl_fork/' $f
+    sed -i -e '/^disable_functions.*=$/ s/$/ exec, shell_exec, system, passthru, popen, proc_open, pcntl_exec, pcntl_fork/' $f
+    sed -i -e '/^disable_functions.*[a-zA-Z0-9],$/ s/$/exec,shell_exec,system,passthru,popen,proc_open,pcntl_exec,pcntl_fork,/' $f
+    sed -i -e '/^disable_functions.*, $/ s/$/exec, shell_exec, system, passthru, popen, proc_open, pcntl_exec, pcntl_fork/' $f
+done < ./data-files/php-locations.txt
 
 # now use sed to edit the disable_functions line
 
