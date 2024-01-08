41fcc7d43b7c465c7527f2dbac26c6fadcc4cb5c
kaichye <kaichengye11@gmail.com>
2024-01-07 20:43:30 -0500

    clean up pansophy

--- linux-scripts/pansophy.sh
+++ linux-scripts/pansophy.sh
@@ -209,51 +209,6 @@ else
     printf "${warn}Session \"$SESSIONW\" already exists!${reset}\n"
 fi
 
-exit 0
-
-# apt update and yum's equivalent
-if [[ "$PKG_MAN" == "apt-get" ]]
-then
-    apt-get update -y &
-    UPDATE_PID=$!
-else
-    yum clean expire-cache -y
-    yum check-update -y &
-    UPDATE_PID=$!
-fi
-
-# av
-# wait for update to finish
-wait $UPDATE_PID
-if [[ "$PKG_MAN" == "apt-get" ]]
-then
-    apt-get install rkhunter -y --force-yes
-else
-    yum install rkhunter -y
-fi
-
-rkhunter --check --sk
-printf "${info}Scan complete, check /var/log/rkhunter.log for results${reset}\n"
-
-
-# nmap scan self
-# wait for update to finish
-wait $UPDATE_PID
-
-if [[ "$PKG_MAN" == "apt-get" ]]
-then
-    apt-get install nmap -y --force-yes
-else
-    yum install nmap -y
-fi
-
-tmux send-keys -t "nmap" "./script-dependencies/quick-scan.sh" C-m
-
-# upgrade
-./script-dependencies/osupdater.sh
-# backup again after update
-./script-dependencies/backup.sh
-
 
 printf "\n${info}Pansophy complete. Are your eyes open?${reset}\n\n"
 
