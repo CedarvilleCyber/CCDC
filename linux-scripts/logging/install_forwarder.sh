2cc5bd9753ad1ec639eb747918ce5cb268ae8d46
kaichye <kaichengye11@gmail.com>
2024-02-29 14:09:17 -0500

    fix up splunk again

--- linux-scripts/logging/install_forwarder.sh
+++ linux-scripts/logging/install_forwarder.sh
@@ -7,12 +7,12 @@ then
 fi
 
 #Install
-#useradd -m splunk
-useradd -m splunkfwd
-#groupadd splunk
-groupadd splunkfwd
-#chown -R splunk:splunk $SPLUNK_HOME
-chown -R splunkfwd:splunkfwd $SPLUNK_HOME
+useradd -m splunk
+#useradd -m splunkfwd
+groupadd splunk
+#groupadd splunkfwd
+chown -R splunk:splunk $SPLUNK_HOME
+#chown -R splunkfwd:splunkfwd $SPLUNK_HOME
 export SPLUNK_HOME=/opt/splunkforwarder
 mkdir $SPLUNK_HOME
 
@@ -25,7 +25,7 @@ do
 	case $flavor in
 		"Debian")
 			#mv splunkforwarder-9.0.1-82c987350fde-linux-2.6-amd64.deb $SPLUNK_HOME
-            wget -O splunkforwarder.deb "https://download.splunk.com/products/universalforwarder/releases/9.2.0.1/linux/splunkforwarder-9.2.0.1-d8ae995bf219-linux-2.6-amd64.deb"
+            wget -O splunkforwarder.deb "https://download.splunk.com/products/universalforwarder/releases/9.0.1/linux/splunkforwarder-9.0.1-82c987350fde-linux-2.6-amd64.deb"
 			mv splunkforwarder.deb $SPLUNK_HOME
 			cd $SPLUNK_HOME
 			#dpkg -i splunkforwarder-9.0.1-82c987350fde-linux-2.6-amd64.deb
@@ -33,7 +33,7 @@ do
 			break;;
 		"RedHat")
 			#chmod 644 splunkforwarder-9.0.1-82c987350fde-linux-2.6-x86_64.rpm
-            wget -O splunkforwarder.rpm "https://download.splunk.com/products/universalforwarder/releases/9.2.0.1/linux/splunkforwarder-9.2.0.1-d8ae995bf219.x86_64.rpm"
+            wget -O splunkforwarder.rpm "https://download.splunk.com/products/universalforwarder/releases/9.0.1/linux/splunkforwarder-9.0.1-82c987350fde-linux-2.6-x86_64.rpm"
 			chmod 644 splunkforwarder.rpm
 			#mv splunkforwarder-9.0.1-82c987350fde-linux-2.6-x86_64.rpm $SPLUNK_HOME
 			mv splunkforwarder.rpm $SPLUNK_HOME
