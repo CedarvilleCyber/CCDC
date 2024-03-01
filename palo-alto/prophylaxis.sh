1c0845aaf179afaf09541b0d2ebf0a0e9bb5d57c
kaichye <kaichengye11@gmail.com>
2024-03-01 12:20:30 -0500

    start prophylaxis

---
+++ palo-alto/prophylaxis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# 
+# prophylaxis.sh
+# 
+# Prophylaxis - Measures designed to preserve health and guard against disease.
+#
+# Palo alto setup script
+# 
+# Kaicheng Ye
+# Mar. 2024
+
+printf "Starting prophylaxis script\n"
+
+# get team ip
+printf "Enter team IP number should be between (21-40): "
+read team
+
+echo "set cli scripting-mode on" > temp.txt
+echo "configure" >> temp.txt
+echo "set address fedora ip-netmask 172.25.$team.39" >> temp.txt
+echo "set address splunk ip-netmask 172.25.$team.9" >> temp.txt
+echo "set address centos ip-netmask 172.25.$team.11" >> temp.txt
+echo "set address debian ip-netmask 172.25.$team.20" >> temp.txt
+echo "set address windows-server ip-netmask 172.25.$team.27" >> temp.txt
+echo "set address win10 ip-netmask 172.25.$team.5" >> temp.txt
+echo "set address this-fw ip-netmask 172.25.$team.2" >> temp.txt
+cat ./prophylaxis.txt >> temp.txt
+cp ./prophylaxis.txt ./backup-prophylaxis.txt
+mv temp.txt prophylaxis.txt
+
+
+exit 1
+
+ssh -T admin@172.20.242.150 < ./prophylaxis.txt
+
+
+
+exit 0
