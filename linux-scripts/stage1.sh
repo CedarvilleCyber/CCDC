5ea4888b34c3afd46e8d5d7e9d7ab0e15e6bfb22
kaichye <kaichengye11@gmail.com>
2024-11-03 17:30:37 -0500

    added curl to stage1

--- linux-scripts/stage1.sh
+++ linux-scripts/stage1.sh
@@ -62,77 +62,220 @@ printf "${error}ERROR: Enter respective name according to machine's purpose:
     or no parameters for generic${reset}\n"; exit 1 ;;
 esac
 
-if [[ $FORCE -eq 0 ]]
+# check if wget is installed
+which wget > /dev/null
+if [[ $? -eq 1 ]]
 then
-    which tar > /dev/null
-    if [[ $? -eq 0 ]]
+
+    # wget is installed
+    # force means insecure (don't check certificate)
+    if [[ $FORCE -eq 0 ]]
     then
-	wget https://github.com/CedarvilleCyber/CCDC/archive/main.tar.gz -O main.tar.gz
-	tar -zxvf ./main.tar.gz
-	cd ./CCDC-main/linux-scripts
-	./pansophy.sh "$MACHINE"
-    else
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/pansophy.sh -O pansophy.sh
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/backup.sh -O backup.sh
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/check-cron.sh -O check-cron.sh
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -O firewall.sh
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -O restore-backup.sh
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -O secure-os.sh
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -O prophylaxis.txt
-	wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -O prophylaxis.sh
+    # NO force
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+            if [[ "$PKG_MAN" == "apt-get" ]]
+            then
+                apt-get update
+                apt-get install tar -y --force-yes
+            else
+                yum clean expire-cache
+                yum check-update
+                yum install tar -y
+            fi
+        fi
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+    	    wget https://github.com/CedarvilleCyber/CCDC/archive/main.tar.gz -O main.tar.gz
+    	    tar -zxvf ./main.tar.gz
+    	    cd ./CCDC-main/linux-scripts
+    	    ./pansophy.sh "$MACHINE"
+        else
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/pansophy.sh -O pansophy.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/backup.sh -O backup.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/check-cron.sh -O check-cron.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -O firewall.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -O restore-backup.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -O secure-os.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -O prophylaxis.txt
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -O prophylaxis.sh
 
-	chmod 700 pansophy.sh
-	chmod 700 backup.sh
-	chmod 700 check-cron.sh
-	chmod 700 firewall.sh
-	chmod 700 restore-backup.sh
-	chmod 700 secure-os.sh
-	chmod 700 propylaxis.sh
+    	    chmod 700 pansophy.sh
+    	    chmod 700 backup.sh
+    	    chmod 700 check-cron.sh
+    	    chmod 700 firewall.sh
+    	    chmod 700 restore-backup.sh
+    	    chmod 700 secure-os.sh
+    	    chmod 700 propylaxis.sh
 
-	# check cron now!
-	./check-cron.sh
+    	    # check cron now!
+    	    ./check-cron.sh
 
-	#./pansophy.sh "$MACHINE" "stage1"
-	git clone https://github.com/CedarvilleCyber/CCDC.git --depth 1
+    	    #./pansophy.sh "$MACHINE" "stage1"
+    	    git clone https://github.com/CedarvilleCyber/CCDC.git --depth 1
 
-	printf "\n\nBasics secured. Now,   cd ./work/CCDC/linux-scripts
+    	    printf "\n\nBasics secured. Now,   cd ./work/CCDC/linux-scripts
 and run pansophy.sh like normal\n\n\n"
+        fi
+    else
+    # YES force
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+            if [[ "$PKG_MAN" == "apt-get" ]]
+            then
+                apt-get update
+                apt-get install tar -y --force-yes
+            else
+                yum clean expire-cache
+                yum check-update
+                yum install tar -y
+            fi
+        fi
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+    	    wget --no-check-certificate https://github.com/CedarvilleCyber/CCDC/archive/main.tar.gz -O main.tar.gz
+    	    tar -zxvf ./main.tar.gz
+    	    cd ./CCDC-main/linux-scripts
+	        ./pansophy.sh "$MACHINE"
+        else
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/pansophy.sh -O pansophy.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/backup.sh -O backup.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/check-cron.sh -O check-cron.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -O firewall.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -O restore-backup.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -O secure-os.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -O prophylaxis.txt
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -O prophylaxis.sh
+
+	        chmod 700 pansophy.sh
+	        chmod 700 backup.sh
+	        chmod 700 check-cron.sh
+	        chmod 700 firewall.sh
+        	chmod 700 restore-backup.sh
+        	chmod 700 secure-os.sh
+        	chmod 700 propylaxis.sh
+
+        	# check cron now!
+        	./check-cron.sh
+
+        	#./pansophy.sh "$MACHINE" "stage1"
+        	git clone https://github.com/CedarvilleCyber/CCDC.git --depth 1
+
+        	printf "\n\nBasics secured. Now,   cd ./work/CCDC/linux-scripts
+and run pansophy.sh like normal\n\n\n"
+        fi
     fi
+
+
 else
-    which tar > /dev/null
-    if [[ $? -eq 0 ]]
+
+    # wget is NOT installed using curl
+    # force means insecure (don't check certificate)
+    if [[ $FORCE -eq 0 ]]
     then
-	wget --no-check-certificate https://github.com/CedarvilleCyber/CCDC/archive/main.tar.gz -O main.tar.gz
-	tar -zxvf ./main.tar.gz
-	cd ./CCDC-main/linux-scripts
-	./pansophy.sh "$MACHINE"
-    else
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/pansophy.sh -O pansophy.sh
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/backup.sh -O backup.sh
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/check-cron.sh -O check-cron.sh
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -O firewall.sh
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -O restore-backup.sh
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -O secure-os.sh
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -O prophylaxis.txt
-	wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -O prophylaxis.sh
+    # NO force
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+            if [[ "$PKG_MAN" == "apt-get" ]]
+            then
+                apt-get update
+                apt-get install tar -y --force-yes
+            else
+                yum clean expire-cache
+                yum check-update
+                yum install tar -y
+            fi
+        fi
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+    	    curl -L https://github.com/CedarvilleCyber/CCDC/archive/main.tar.gz -o main.tar.gz
+    	    tar -zxvf ./main.tar.gz
+    	    cd ./CCDC-main/linux-scripts
+    	    ./pansophy.sh "$MACHINE"
+        else
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/pansophy.sh -o pansophy.sh
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/backup.sh -o backup.sh
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/check-cron.sh -o check-cron.sh
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -o firewall.sh
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -o restore-backup.sh
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -o secure-os.sh
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -o prophylaxis.txt
+    	    curl -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -o prophylaxis.sh
 
-	chmod 700 pansophy.sh
-	chmod 700 backup.sh
-	chmod 700 check-cron.sh
-	chmod 700 firewall.sh
-	chmod 700 restore-backup.sh
-	chmod 700 secure-os.sh
-	chmod 700 propylaxis.sh
+    	    chmod 700 pansophy.sh
+    	    chmod 700 backup.sh
+    	    chmod 700 check-cron.sh
+    	    chmod 700 firewall.sh
+    	    chmod 700 restore-backup.sh
+    	    chmod 700 secure-os.sh
+    	    chmod 700 propylaxis.sh
 
-	# check cron now!
-	./check-cron.sh
+    	    # check cron now!
+    	    ./check-cron.sh
 
-	#./pansophy.sh "$MACHINE" "stage1"
-	git clone https://github.com/CedarvilleCyber/CCDC.git --depth 1
+    	    #./pansophy.sh "$MACHINE" "stage1"
+    	    git clone https://github.com/CedarvilleCyber/CCDC.git --depth 1
 
-	printf "\n\nBasics secured. Now,   cd ./work/CCDC/linux-scripts
+    	    printf "\n\nBasics secured. Now,   cd ./work/CCDC/linux-scripts
 and run pansophy.sh like normal\n\n\n"
+        fi
+    else
+    # YES force
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+            if [[ "$PKG_MAN" == "apt-get" ]]
+            then
+                apt-get update
+                apt-get install tar -y --force-yes
+            else
+                yum clean expire-cache
+                yum check-update
+                yum install tar -y
+            fi
+        fi
+        which tar > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+    	    curl -k -L https://github.com/CedarvilleCyber/CCDC/archive/main.tar.gz -o main.tar.gz
+    	    tar -zxvf ./main.tar.gz
+    	    cd ./CCDC-main/linux-scripts
+	        ./pansophy.sh "$MACHINE"
+        else
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/pansophy.sh -o pansophy.sh
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/backup.sh -o backup.sh
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/check-cron.sh -o check-cron.sh
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -o firewall.sh
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -o restore-backup.sh
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -o secure-os.sh
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -o prophylaxis.txt
+    	    curl -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -o prophylaxis.sh
+
+	        chmod 700 pansophy.sh
+	        chmod 700 backup.sh
+	        chmod 700 check-cron.sh
+	        chmod 700 firewall.sh
+        	chmod 700 restore-backup.sh
+        	chmod 700 secure-os.sh
+        	chmod 700 propylaxis.sh
+
+        	# check cron now!
+        	./check-cron.sh
+
+        	#./pansophy.sh "$MACHINE" "stage1"
+        	git clone https://github.com/CedarvilleCyber/CCDC.git --depth 1
+
+        	printf "\n\nBasics secured. Now,   cd ./work/CCDC/linux-scripts
+and run pansophy.sh like normal\n\n\n"
+        fi
     fi
+
 fi
 
 exit 0
