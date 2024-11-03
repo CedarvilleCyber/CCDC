192dd7869c186920c0f811f5cff82d3228a40169
kaichye <kaichengye11@gmail.com>
2024-11-03 16:54:01 -0500

    new eol fix file

---
+++ linux-scripts/eol-mirrors.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 
+# eol-mirrors.sh
+# 
+# fixes end of life package mirrors. Like CentOS 7
+# 
+# Kaicheng Ye
+# Nov. 2024
+
+if [[ "$(id -u)" != "0" ]]
+then
+    printf "${error}ERROR: The script must be run with sudo privileges!${reset}\n"
+    exit 1
+fi
+
+printf "${info}Starting eol-mirrors fix script${reset}\n"
+
+# Set up some environment variables
+. /etc/os-release
+
+# check for CentOS 7
+if [[ "$ID" == "centos" && $VERSION_ID -le 7 ]]
+then
+    # mirrors file
+    # /etc/yum.repos.d/CentOS-Base.repo
+
+    sed -ie '/mirrorlisti=http/ s/^/#/' /etc/yum.repos.d/CentOS-Base.repo
+    sed -ie '/#baseurl/ s/^#//' /etc/yum.repos.d/CentOS-Base.repo
+    sed -ie '/baseurl/ s/mirror.centos.org/vault.centos.org/' /etc/yum.repos.d/CentOS-Base.repo
+fi
+
+exit 0
