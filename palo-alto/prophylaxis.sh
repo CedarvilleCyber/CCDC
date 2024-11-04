2dacf09fb0d5359e63757bfc147866bd2334633f
kaichye <kaichengye11@gmail.com>
2024-11-04 09:47:39 -0500

    PA change names

--- palo-alto/prophylaxis.sh
+++ palo-alto/prophylaxis.sh
@@ -17,13 +17,17 @@ read team
 
 echo "set cli scripting-mode on" > temp.txt
 echo "configure" >> temp.txt
-echo "set address fedora ip-netmask 172.25.$team.39" >> temp.txt
-echo "set address splunk ip-netmask 172.25.$team.9" >> temp.txt
-echo "set address centos ip-netmask 172.25.$team.11" >> temp.txt
-echo "set address debian ip-netmask 172.25.$team.20" >> temp.txt
-echo "set address windows-server ip-netmask 172.25.$team.27" >> temp.txt
-echo "set address win10 ip-netmask 172.31.$team.5" >> temp.txt
+echo "set address public-fedora ip-netmask 172.25.$team.39" >> temp.txt
+echo "set address public-splunk ip-netmask 172.25.$team.9" >> temp.txt
+echo "set address public-centos ip-netmask 172.25.$team.11" >> temp.txt
+echo "set address public-debian ip-netmask 172.25.$team.20" >> temp.txt
+echo "set address public-ubuntu-web ip-netmask 172.25.$team.23" >> temp.txt
+echo "set address public-windows-server ip-netmask 172.25.$team.27" >> temp.txt
+echo "set address public-windows-docker ip-netmask 172.25.$team.97" >> temp.txt
+echo "set address public-win10 ip-netmask 172.31.$team.5" >> temp.txt
+echo "set address public-ubuntu-wkst ip-netmask 172.25.$team.111" >> temp.txt
 echo "set address this-fw ip-netmask 172.31.$team.2" >> temp.txt
+
 cat ./prophylaxis.txt >> temp.txt
 cp ./prophylaxis.txt ./backup-prophylaxis.txt
 mv temp.txt prophylaxis.txt
