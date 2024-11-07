c9b11ec81d57a4ae68fd63bdbf14895a11dd976e
kaichye <kaichengye11@gmail.com>
2024-11-07 14:12:01 -0500

    make secure-os backgroundable

--- linux-scripts/secure-os.sh
+++ linux-scripts/secure-os.sh
@@ -40,53 +40,56 @@ else
 fi
 
 
-# change file permissions
-# executable only for .sh extensions.
-# user only permissions except for on directories.
-find /home -type f > ./data-files/files.txt
-find /home -type d -exec chmod 755 {} +
-find /root -type f >> ./data-files/files.txt
-find /root -type d -exec chmod 755 {} +
-chmod 644 /etc/passwd
-chmod 640 /etc/shadow
-chmod 440 /etc/sudoers
-
-
-# reads through each line of a file, ignoring whitespace
-while IFS="" read -r f || [[ -n "$f" ]]
-do
-    read -r line < $f
-    if [[ "$line" == "#!/bin/bash" ]]
-    then
-        chmod 700 $f
-    elif [[ "$line" == "#!/bin/sh" ]]
-    then
-        chmod 700 $f
-    else
-        chmod 600 $f
-    fi
-
-done < ./data-files/files.txt
-
-
-# remove uneccesary applications
-$PKG_MAN remove nc -y
-$PKG_MAN remove netcat -y
-$PKG_MAN remove ncat -y
-
-# remove uneccesary services
-which systemctl >/dev/null
-if [[ $? -eq 0 ]]
+if [[ "$1" != "background" ]]
 then
-    # cups is a printing service
-    # speech-dispatcher is some tts thing
-    systemctl stop cups
-    systemctl disable cups
-    systemctl stop speech-dispatcher
-    systemctl disable speech-dispatcher
-else
-    service cups stop
-    service speech-dispatcher stop
+    # change file permissions
+    # executable only for .sh extensions.
+    # user only permissions except for on directories.
+    find /home -type f > ./data-files/files.txt
+    find /home -type d -exec chmod 755 {} +
+    find /root -type f >> ./data-files/files.txt
+    find /root -type d -exec chmod 755 {} +
+    chmod 644 /etc/passwd
+    chmod 640 /etc/shadow
+    chmod 440 /etc/sudoers
+
+    # reads through each line of a file, ignoring whitespace
+    while IFS="" read -r f || [[ -n "$f" ]]
+    do
+        read -r line < $f
+        if [[ "$line" == "#!/bin/bash" ]]
+        then
+            chmod 700 $f
+        elif [[ "$line" == "#!/bin/sh" ]]
+        then
+            chmod 700 $f
+        else
+            chmod 600 $f
+        fi
+
+    done < ./data-files/files.txt
+
+
+    # remove uneccesary applications
+    $PKG_MAN remove nc -y
+    $PKG_MAN remove netcat -y
+    $PKG_MAN remove ncat -y
+
+
+    # remove uneccesary services
+    which systemctl >/dev/null
+    if [[ $? -eq 0 ]]
+    then
+        # cups is a printing service
+        # speech-dispatcher is some tts thing
+        systemctl stop cups
+        systemctl disable cups
+        systemctl stop speech-dispatcher
+        systemctl disable speech-dispatcher
+    else
+        service cups stop
+        service speech-dispatcher stop
+    fi
 fi
 
 # move pre authorized keys
@@ -113,46 +116,52 @@ do
     rm -rf "$file"e
 done
 
-# stop php web shells
-# First find all php.ini file locations
-find / -iname "php.ini" > ./data-files/php-locations.txt 2>/dev/null
 
-counter=0
-# reads through each line of a file, ignoring whitespace
-while IFS="" read -r f || [[ -n "$f" ]]
-do
-    printf "${info}$f${reset}\n"
-
-    # create backups before writing over the file
-    name=`echo $f | sed 's/\//_/g'`
-    cp $f /usr/bak/$name
-    ((counter++))
-
-    # now use sed to edit the disable_functions line
-    # Checks for different "states" of the "disable_functions" line
-    sed -i -e '/^disable_functions.*[a-zA-Z0-9]$/ s/$/,exec,shell_exec,system,passthru,popen,proc_open,pcntl_exec,pcntl_fork,curl_exec,curl_exec_multi,phpinfo,mail,mb_send_mail,dl/' $f
-    sed -i -e '/^disable_functions.*=$/ s/$/ exec, shell_exec, system, passthru, popen, proc_open, pcntl_exec, pcntl_fork, curl_exec, curl_exec_multi, phpinfo, mail, mb_send_mail, dl/' $f
-    sed -i -e '/^disable_functions.*[a-zA-Z0-9],$/ s/$/exec,shell_exec,system,passthru,popen,proc_open,pcntl_exec,pcntl_fork,curl_exec,curl_exec_multi,phpinfo,mail,mb_send_mail,dl,/' $f
-    sed -i -e '/^disable_functions.*, $/ s/$/exec, shell_exec, system, passthru, popen, proc_open, pcntl_exec, pcntl_fork, curl_exec, curl_exec_multi, phpinfo, mail, mb_send_mail, dl/' $f
-done < ./data-files/php-locations.txt
-
-# Restart apache2/httpd or nginx if we changed php
-# Just try everything. No need to test one at a time
-if [[ $counter -ne 0 ]]
+if [[ "$1" != "background" ]]
 then
-    printf "${info}Restarting Web Server${reset}\n"
-    which systemctl >/dev/null
-    if [[ $? -eq 0 ]]
+    # stop php web shells
+    # First find all php.ini file locations
+    find / -iname "php.ini" > ./data-files/php-locations.txt 2>/dev/null
+
+    counter=0
+    # reads through each line of a file, ignoring whitespace
+    while IFS="" read -r f || [[ -n "$f" ]]
+    do
+        printf "${info}$f${reset}\n"
+
+        # create backups before writing over the file
+        name=`echo $f | sed 's/\//_/g'`
+        cp $f /usr/bak/$name
+        ((counter++))
+
+        # now use sed to edit the disable_functions line
+        # Checks for different "states" of the "disable_functions" line
+        sed -i -e '/^disable_functions.*[a-zA-Z0-9]$/ s/$/,exec,shell_exec,system,passthru,popen,proc_open,pcntl_exec,pcntl_fork,curl_exec,curl_exec_multi,phpinfo,mail,mb_send_mail,dl/' $f
+        sed -i -e '/^disable_functions.*=$/ s/$/ exec, shell_exec, system, passthru, popen, proc_open, pcntl_exec, pcntl_fork, curl_exec, curl_exec_multi, phpinfo, mail, mb_send_mail, dl/' $f
+        sed -i -e '/^disable_functions.*[a-zA-Z0-9],$/ s/$/exec,shell_exec,system,passthru,popen,proc_open,pcntl_exec,pcntl_fork,curl_exec,curl_exec_multi,phpinfo,mail,mb_send_mail,dl,/' $f
+        sed -i -e '/^disable_functions.*, $/ s/$/exec, shell_exec, system, passthru, popen, proc_open, pcntl_exec, pcntl_fork, curl_exec, curl_exec_multi, phpinfo, mail, mb_send_mail, dl/' $f
+    done < ./data-files/php-locations.txt
+
+
+    # Restart apache2/httpd or nginx if we changed php
+    # Just try everything. No need to test one at a time
+    if [[ $counter -ne 0 ]]
     then
-        systemctl restart apache2
-        systemctl restart httpd
-        systemctl restart nginx
-    else
-        service apache2 restart
-        service httpd restart
-        service nginx restart
+        printf "${info}Restarting Web Server${reset}\n"
+        which systemctl >/dev/null
+        if [[ $? -eq 0 ]]
+        then
+            systemctl restart apache2
+            systemctl restart httpd
+            systemctl restart nginx
+        else
+            service apache2 restart
+            service httpd restart
+            service nginx restart
+        fi
     fi
 fi
+
 
 # suid and sgid
 while IFS="" read -r line || [ -n "$line" ]
@@ -229,11 +238,13 @@ then
     printf "Then, come back and rerun pansophy like normal.${reset}\n\n"
 fi
 
-if [[ "$FOUND" != "" || "$RC" != "" ]]
+if [[ "$1" != "background" ]]
 then
-    exit 0
-    #printf "${warn}Ignore above if run with pansophy.sh. Restart required later anyway.${reset}\n"
+    if [[ "$FOUND" != "" || "$RC" != "" ]]
+    then
+        exit 0
+        #printf "${warn}Ignore above if run with pansophy.sh. Restart required later anyway.${reset}\n"
+    fi
 fi
-
 
 exit 0
