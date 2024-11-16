2c36e2c8abbcfc5035dd2897fdb6656049725cbf
Logan Miller <lj@mountainmillers.com>
2024-11-15 22:34:16 -0500

    Started dashboard script

---
+++ docker/service-dashboards.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ECOMM_SES="ecomm-state"
+WEBMAIL_SES="webmail-state"
+BIND9_SES="bind9-state"
+SPLUNK_SES="splunk-state"
+
+WIN_NAME="bash"
+KILL="false"
+
+# Set up ecomm-state dashboard
+SESSION_NAME=$ECOMM_SES
+tmux has-session -t $SESSION_NAME &> /dev/null
+if [[ "$?" == "0" ]]; then
+	read -p "Dashboard for ecomm-state already exists. Do you want to kill it? [y/n] " REPLACE
+	if [[ "$REPLACE" == "y" ]]; then
+		KILL=true
+	fi
+fi
+
+if [[ "$KILL" ]]; then
+	tmux kill-session -t $SESSION_NAME
+	KILL=false
+
+else
+	tmux new-session -d -s $SESSION_NAME -n $WIN_NAME
+	sleep 1
+	
+	tmux split-window -h -t $SESSION_NAME:0
+	tmux split-window -v -t 1
+	sleep 1
+	
+	tmux send-keys -t 0 "docker ps" Enter
+	tmux send-keys -t 1 "docker exec -it THINGY bash" Enter # FIXME
+	tmux send-keys -t 2 "docker log -f THINGY" Enter # FIXME
+fi
+
