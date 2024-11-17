e1db446c666d17e7bb626f324190a274a5367e41
MtSneffels <113790394+MtSneffels@users.noreply.github.com>
2024-11-16 22:42:45 -0500

    Update service-dashboards.sh

--- docker/service-dashboards.sh
+++ docker/service-dashboards.sh
@@ -5,24 +5,23 @@ WEBMAIL_SES="webmail-state"
 BIND9_SES="bind9-state"
 SPLUNK_SES="splunk-state"
 
-WIN_NAME="bash"
-KILL="false"
+WIN_NAME="dashboard"
+QUIT="false"
 
 # Set up ecomm-state dashboard
 SESSION_NAME=$ECOMM_SES
 tmux has-session -t $SESSION_NAME &> /dev/null
 if [[ "$?" == "0" ]]; then
 	read -p "Dashboard for ecomm-state already exists. Do you want to kill it? [y/n] " REPLACE
-	if [[ "$REPLACE" == "y" ]]; then
-		KILL=true
-	fi
+	if [[ "$REPLACE" != "y" ]]; then
+		QUIT="true"
+	else
+ 		tmux kill-session -t $SESSION_NAME
+   	fi
 fi
 
-if [[ "$KILL" ]]; then
-	tmux kill-session -t $SESSION_NAME
-	KILL=false
+if [[ "$QUIT" != "true" ]]; then
 
-else
 	tmux new-session -d -s $SESSION_NAME -n $WIN_NAME
 	sleep 1
 	
