accdf7e7fa7f91d5256a9a8892636e8959118761
Logan Miller <lj@mountainmillers.com>
2024-12-30 18:25:40 -0800

    Finished second dashboard script

--- docker/watchfulwhale.sh
+++ docker/watchfulwhale.sh
@@ -3,7 +3,8 @@
 # Author: Logan Miller
 # Date: 12/30/2024
 # File: watchfulwhale.sh
-# Status: Nothing done
+# Status: Tested and works, but display is weird if tmux send-keys ends
+# up being over 79 characters.
 #
 # Purpose: Create a single admin tmux session with one window per scored
 # docker service.
@@ -38,51 +39,53 @@ if [[ "$QUIT" != "true" ]]; then
 	WIN_NAME=$ECOMM_WIN
 	tmux new-window -n $WIN_NAME
 	sleep 1
-	tmux split-window -h -t $SESSION_NAME:1
-	
+	tmux split-window -h -t $SESSION_NAME:1	
 	
 	sleep .4
-	tmux send-keys -t 1 "cd $WIN_NAME" Enter
+	tmux send-keys -t .+ "cd $WIN_NAME; clear" Enter
 	sleep .4
 	tmux send-keys -t 1 "docker exec -it prestashop bash"
-
-	sleep .4
-	tmux send-keys -t 0 "cd $WIN_NAME" Enter
 	sleep .4
 	tmux send-keys -t 0 "./setup-ecomm.sh"
 
-	# # Set up webmail-state window
-	# WIN_NAME=$WEBMAIL_WIN
-	# tmux new-window -n $WIN_NAME
-	# sleep 1
-	# tmux split-window -h -t $SESSION_NAME:2
+	# Set up webmail-state window
+	WIN_NAME=$WEBMAIL_WIN
+	tmux new-window -n $WIN_NAME
+	sleep 1
+	tmux split-window -h -t $SESSION_NAME:2
 	
-	# # Need to fix
-	# # tmux send-keys -t 0 "clear" Enter
-	# # tmux send-keys -t 0 "cd $WIN_NAME; ./setup-ecomm.sh"
-	# # tmux send-keys -t 1 "cd $WIN_NAME; docker exec -it prestashop bash"
+	sleep .4
+	tmux send-keys -t .+ "cd $WIN_NAME; clear" Enter
+	sleep .4
+	tmux send-keys -t 1 "docker exec -it webmail bash"
+	sleep .4
+	tmux send-keys -t 0 "1st cmd for webmail"
 
-	# # Set up ecomm-state window
-	# WIN_NAME=$BIND9_WIN
-	# tmux new-window -n $WIN_NAME
-	# sleep 1
-	# tmux split-window -h -t $SESSION_NAME:3
+	# Set up bind9-state window
+	WIN_NAME=$BIND9_WIN
+	tmux new-window -n $WIN_NAME
+	sleep 1
+	tmux split-window -h -t $SESSION_NAME:3
 	
-	# # Need to fix
-	# # tmux send-keys -t 0 "clear" Enter
-	# # tmux send-keys -t 0 "cd $WIN_NAME; ./setup-ecomm.sh"
-	# # tmux send-keys -t 1 "cd $WIN_NAME; docker exec -it prestashop bash"
+	sleep .4
+	tmux send-keys -t .+ "cd $WIN_NAME; clear" Enter
+	sleep .4
+	tmux send-keys -t 1 "docker exec -it bind9 bash"
+	sleep .4
+	tmux send-keys -t 0 "1st cmd for bind9"
 
-	# # Set up ecomm-state window
-	# WIN_NAME=$SPLUNK_WIN
-	# tmux new-window -n $WIN_NAME
-	# sleep 1
-	# tmux split-window -h -t $SESSION_NAME:4
+	# Set up splunk-state window
+	WIN_NAME=$SPLUNK_WIN
+	tmux new-window -n $WIN_NAME
+	sleep 1
+	tmux split-window -h -t $SESSION_NAME:4
 	
-	# # Need to fix
-	# # tmux send-keys -t 0 "clear" Enter
-	# # tmux send-keys -t 0 "cd $WIN_NAME; ./setup-ecomm.sh"
-	# # tmux send-keys -t 1 "cd $WIN_NAME; docker exec -it prestashop bash"
+	sleep .4
+	tmux send-keys -t .+ "cd $WIN_NAME; clear" Enter
+	sleep .4
+	tmux send-keys -t 1 "docker exec -it splunk bash"
+	sleep .4
+	tmux send-keys -t 0 "1st cmd for splunk"
 
 	printf "\e[0;32mSuccessfully generated $SESSION_NAME dashboard\e[0m\n"
 fi
