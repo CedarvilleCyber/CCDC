82a781cf55610cc6b34a666ce47315a436af3843
kaichye <kaichengye11@gmail.com>
2025-01-02 15:13:02 -0500

    overwrite login banner

--- linux-scripts/login-banners.sh
+++ linux-scripts/login-banners.sh
@@ -17,7 +17,7 @@ chattr -i -a /etc/ssh/sshd-banner
 chattr -i -a /etc/ssh/sshd_config
 
 BANNER="Warning: Only authorized users are permitted to login. All network activity is being monitored and logged, and may be used to investigate and prosecute any instance of unauthorized access."
-echo $BANNER | tee -a /etc/issue /etc/issue.net > /dev/null
+echo $BANNER | tee /etc/issue /etc/issue.net > /dev/null
 echo $BANNER | tee /etc/ssh/sshd-banner > /dev/null
 echo "Banner /etc/ssh/sshd-banner" | tee -a /etc/ssh/sshd_config > /dev/null
 
