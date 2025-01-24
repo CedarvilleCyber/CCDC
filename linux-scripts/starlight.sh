b52ef20ed560c12ad698c7ef16695ab240ada8bb
Logan Miller <lj@mountainmillers.com>
2025-01-23 20:14:23 -0500

    updated starlight, but not complete

--- linux-scripts/starlight.sh
+++ linux-scripts/starlight.sh
@@ -4,165 +4,134 @@
 #
 # Author: Logan Miller
 #
-# Dependencies:
-# * configs/lmiller/.tmux.conf
-# * configs/lmiller/.vimrc
-#
-# Notes:
-# * check out chroot jail for prestashop folder
+# TODO:
+#   - chattr all but the cache (remove webshells first)
+#   - replace httpd.conf and php.ini
 #
 
-# Starlight must be run as root
+# Advise user to reinstall prestashop -----------------------------------------
+printf $'\e[0;31mIt is recommended that you reinstall prestashop and run mysql_secure_installation over using this script.\e[0m\n'
+read -p $'\e[0;31mWould you like to continue? [y/n] \e[0m' CONT
+if [[ "$CONT" != "y" ]]; then
+	exit 0
+fi
+
+# Script must be run as root --------------------------------------------------
 if [ $(id -u) != 0 ]; then
-    echo "You must use sudo to run this script!"
+    printf $'\e[0;31mYou must be sudo to run this script!\e[0m\n'
     exit 1
 fi
 
-# General Security Operations --------------------------------------------------
-# Install utilities
-echo "Installing utilities ..."
+# Install utilities -----------------------------------------------------------
+printf $'\e[0;36mInstalling utilities ...\e[0m\n'
+
 yum install -y curl
 yum install -y nmap
 yum install -y mlocate
 yum install -y python3
 
-# Copy configuration files from repo
-echo "Moving config files ..."
-cp ./configs/lmiller/.tmux.conf ~/.tmux.conf
-cp ./configs/lmiller/.vimrc ~/.vimrc
-tmux source ~/.tmux.conf
+printf $'\e[0;32mFinished installing utilities\e[0m\n'
 
-# Get working directory
-echo "Your current directory is $(pwd)"
-read -p $'\e[36mEnter your working directory (. for this one): \e[0m' WK_DIR
-if [[ "$WK_DIR" = "." ]]; then
-    WK_DIR=$(pwd)
-fi
-export WK_DIR
+# Secure MySQL database -------------------------------------------------------
+printf $'\e[0;36mSecuring database ...\e[0m\n'
 
-# Get backup directory
-read -p $'\e[36mEnter the full path of the backup directory (e.g., /usr/bak): \e[0m' BK_DIR
-if [[ "$BK_DIR" = "." ]]; then
-    BK_DIR=$(pwd)
-fi
-export BK_DIR
+printf $'\e[0;36mUpdate all passwords and enter yes to all prompts\e[0m\n'
+mysql_secure_installation
 
-# Secure Mysql Database --------------------------------------------------------
-read -p $'\e[36mSecure MySQL? [y/n] \e[0m' sec_mysql
-if [[ "$sec_mysql" = "y" ]]; then
-        echo "Securing MySQL database ..."
-        
-	echo "Change the root password and answer yes to all prompts"
+read -p $'\e[0;36mPress enter to continue\e[0m\n'
 
-	# Run mysql_secure_installation script (provided with mysql installation)
-	mysql_secure_installation
+printf $'\e[0;32mDatabase secured\e[0m\n'
 
-	# ??
-	cat /etc/my.cnf | sed "/\[mysqld\]/ a bind-address=127.0.0.1" > temp && cat temp > /etc/my.cnf
-	rm temp
+# Secure Prestashop -----------------------------------------------------------
+printf $'\e[0;36mSecuring prestashop ...\e[0m\n'
+
+# Get Prestashop install path and version
+read -p $'\e[36mEnter Prestashop install location (likely /var/www/html/prestashop): \e[0m' presta_install_path
+read -p $'\e[36mIs Prestashop <= 1.6? [y/n] \e[0m' presta_1_6_install
+
+if [[ "$presta_1_6_install" = "n" ]]; then
+	config_file="$presta_install_path/app/config/parameters.php"
+else
+	config_file="$presta_install_path/config/settings.inc.php"
 fi
 
-# Secure Prestashop ------------------------------------------------------------
-read -p $'\e[36mSecure Prestashop? [y/n] \e[0m' sec_presta
-if [[ "$sec_presta" = "y" ]]; then
-	echo "Securing MySQL database ..."
-	
-        # Get Prestashop install path and version
-	read -p $'\e[36mEnter Prestashop install location (likely /var/www/html/prestashop): \e[0m' presta_install_path
-	read -p $'\e[36mIs Prestashop <= 1.6? [y/n] \e[0m' presta_1_6_install
-	
-	if [[ "$presta_1_6_install" = "n" ]]; then
-		config_file="$presta_install_path/app/config/parameters.php"
-	else
-		config_file="$presta_install_path/config/settings.inc.php"
-	fi
+# Update admin page url
+read -p $'\e[0;36mPlease provide a new name for the admin page: \e[0m' ADMIN
+find $presta_install_path -maxdepth 1 -name 'admin*' -exec mv {} $presta_install_path/$ADMIN \;
 
-	# Randomize admin panel
-	echo "Randomizing admin panel ..."
-	
-	echo $'\e[1;33mView presta-admin-panel.txt to access the admin panel\e[0m'
-	
-	find $presta_install_path -maxdepth 1 -name 'admin*' -exec mv {} $presta_install_path/admin$RANDOM \;
-	echo "Access the admin panel at " `find $presta_install_path -maxdepth 1 -name 'admin*' -exec echo {} \;` > $WK_DIR/presta-admin-panel.txt
+# Change prestashop database password
+printf $'\e[36mEnter the new database root passwd: \e[0m'
+read -s dbnew1
+echo
+printf $'\e[36mRetype new password: \e[0m'
+read -s dbnew2
+echo
 
-	# Change prestashop database password
-	printf $'\e[36mEnter the new database root passwd: \e[0m'
+# Check for matching new password
+while [[ "$dbnew1" != "$dbnew2" ]]
+do
+	printf $'\e[31mPasswords do not match!\e[0m'
+	printf $'\e[36mEnter new password: \e[0m'
 	read -s dbnew1
 	echo
 	printf $'\e[36mRetype new password: \e[0m'
 	read -s dbnew2
 	echo
+done
 
-	# check for matching new password
-	while [[ "$dbnew1" != "$dbnew2" ]]
-	do
-		printf "Passwords do not match!\n"
-		printf $'\e[36mEnter new password: \e[0m'
-		read -s dbnew1
-		echo
-		printf $'\e[36mRetype new password: \e[0m'
-		read -s dbnew2
-		echo
-	done
+cat $config_file | sed "s/\(_DB_PASSWD_', '\).*\(');\)/\1$dbnew2\2/" > temp && cat temp > $config_file
+rm temp
 
-	echo "Changing prestashop database password ..."
-	cat $config_file | sed "s/\(_DB_PASSWD_', '\).*\(');\)/\1$dbnew2\2/" > temp && cat temp > $config_file
-	rm temp
+db_name=`cat $config_file | grep "_DB_NAME_" | sed "s/define('_DB_NAME_', '\(.*\)');/\1/"`
+db_prefix=`cat $config_file | grep "_DB_PREFIX_" | sed "s/define('_DB_PREFIX_', '\(.*\)');/\1/"`
 
-	db_name=`cat $config_file | grep "_DB_NAME_" | sed "s/define('_DB_NAME_', '\(.*\)');/\1/"`
-	db_prefix=`cat $config_file | grep "_DB_PREFIX_" | sed "s/define('_DB_PREFIX_', '\(.*\)');/\1/"`
+echo $'\e[1;33mListing TABLE ps_employees from DATABASE $db_name\e[0m'
+mysql -u root --password="$dbnew2" "$db_name" --execute="SELECT id_employee as id,firstname,lastname,email from ${db_prefix}employee;"
 
-	read -p $'\e[36mUpdate the prestashop admin password [y/n]? \e[0m' update_presta_pw
-	if [[ "$update_presta_pw" = "y" ]]; then
-		echo $'\e[1;33mListing TABLE ps_employees from DATABASE $db_name\e[0m'
-		mysql -u root --password="$dbnew2" "$db_name" --execute="SELECT id_employee as id,firstname,lastname,email from ${db_prefix}employee;"
+read -p $'\e[36mEnter the admin id: \e[0m' admin_id
 
-		read -p $'\e[36mEnter the admin id: \e[0m' admin_id
+printf $'\e[36mEnter the new admin passwd: \e[0m'
+read -s admnew1
+echo
+printf $'\e[36mRetype new password: \e[0m'
+read -s admnew2
+echo
 
-		printf $'\e[36mEnter the new admin passwd: \e[0m'
-		read -s admnew1
-		echo
-		printf $'\e[36mRetype new password: \e[0m'
-		read -s admnew2
-		echo
+# Validate that passwords match
+while [[ "$admnew1" != "$admnew2" ]]; do
+printf $'\e[31mPasswords do not match!\e[0m'
+printf $'\e[36mEnter new password: \e[0m'
+read -s admnew1
+echo
+printf $'\e[36mRetype new password: \e[0m'
+read -s admnew2
+echo
+done
 
-		# Validate that passwords match
-		while [[ "$admnew1" != "$admnew2" ]]; do
-		printf "Passwords do not match!\n"
-		printf $'\e[36mEnter new password: \e[0m'
-		read -s admnew1
-		echo
-		printf $'\e[36mRetype new password: \e[0m'
-		read -s admnew2
-		echo
-		done
+db_cookie=`cat $config_file | grep "_COOKIE_KEY_" | sed "s/define('_COOKIE_KEY_', '\(.*\)');/\1/"`
+mysql -u root --password="$dbnew2" "$db_name" --execute="UPDATE ${db_prefix}employee SET passwd=MD5('${db_cookie}${admnew2}') WHERE id_employee='$admin_id';"
 
-		echo "Updating $admin_email's passwd ..."
-		db_cookie=`cat $config_file | grep "_COOKIE_KEY_" | sed "s/define('_COOKIE_KEY_', '\(.*\)');/\1/"`
-		mysql -u root --password="$dbnew2" "$db_name" --execute="UPDATE ${db_prefix}employee SET passwd=MD5('${db_cookie}${admnew2}') WHERE id_employee='$admin_id';"
-	fi
-	
-	# Backup /var/www/html/prestashop to /usr/bak/prestashop
-	echo "Backing up prestashop directory"
-	cp -a $presta_install_path $BK_DIR/
-	
-	# Additional prestashop backup in case first is corrupted by monitoring
-	cp -a $presta_install_path $BK_DIR/prestashop_bak
-fi
+printf $'\e[0;32mPrestashop secured\e[0m\n'
 
-# Backup MySQL -----------------------------------------------------------------
-if [[ "$sec_mysql" = "y" ]]; then
-	echo "Backing up MySQL database ..."
-	
-	echo $'\e[1;33mView how to restore the database in sql-backup-instructions.txt\e[0m'
-	
-	mkdir $BK_DIR/sqldump
-	BK_FILE=$BK_DIR/sqldump/${db_name}`date +%T`.sql
-	
-	echo "You can restore the database using mysql -u root -p database_name < $BK_FILE" > $WK_DIR/sql-backup-instructions.txt
-	mysqldump -u root --password="$dbnew2" "$db_name" > $BK_FILE
-fi
+# Perform backups -------------------------------------------------------------
+printf $'\e[0;36mCreating backups ...\e[0m\n'
+read -p $'\e[0;36mWhere would you like backups placed? (ex: /usr/opt) \e[0m' BK_DIR
+
+cp -a $presta_install_path $BK_DIR/prestashop_dirty
+mysqldump -u root --password="$dbnew2" "$db_name" > $BK_DIR/db_dirty
+
+printf $'\e[0;36mOriginal backups made ... actual files should now be examined for malware before a clean backup is made\e[0m\n'
+
+printf $'\e[1;31mWARNING: Remove any webshells from the prestashop directory!\e[0m\n'
+read -p $'\e[0;31mPress Enter to continue once prestashop directory is clean\e[0m' CONT
+
+printf $'\e[1;31mWARNING: Remove any malware from the database using phpmyadmin!\e[0m\n'
+read -p $'\e[0;31mPress Enter to continue once database is clean\e[0m' CONT
+
+cp -a $presta_install_path $BK_DIR/prestashop_clean
+mysqldump -u root --password="$dbnew2" "$db_name" > $BK_DIR/db_clean
+
+printf $'\e[0;32mBackups complete\e[0m\n'
 
 # Script complete! -------------------------------------------------------------
 printf "\e[0;32mStarlight complete! Cast the ashes back in their eyes!\e[0m\n"
-exit 0
