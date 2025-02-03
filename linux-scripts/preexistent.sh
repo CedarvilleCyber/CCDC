0d8d82840dd9e3c4b851f98cfc6f57399cfee05b
arrxnov <70920682+arrxnov@users.noreply.github.com>
2025-02-03 16:58:31 -0500

    One last thing

--- linux-scripts/preexistent.sh
+++ linux-scripts/preexistent.sh
@@ -126,8 +126,8 @@ then
     	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -O firewall.sh
     	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -O restore-backup.sh
     	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -O secure-os.sh
-    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -O prophylaxis.txt
-    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -O prophylaxis.sh
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.txt -O omniscience.txt
+    	    wget --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.sh -O omniscience.sh
 
     	    chmod 700 sovereignty.sh
     	    chmod 700 backup.sh
@@ -176,8 +176,8 @@ and run soveriegnty.sh like normal\n\n\n"
     	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -O firewall.sh
     	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -O restore-backup.sh
     	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -O secure-os.sh
-    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -O prophylaxis.txt
-    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -O prophylaxis.sh
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.txt -O omniscience.txt
+    	    wget --no-check-certificate --no-cache https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.sh -O omniscience.sh
 
 	        chmod 700 sovereignty.sh
 	        chmod 700 backup.sh
@@ -235,8 +235,8 @@ else
     	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -o firewall.sh
     	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -o restore-backup.sh
     	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -o secure-os.sh
-    	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -o prophylaxis.txt
-    	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -o prophylaxis.sh
+    	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.txt -o omniscience.txt
+    	    curl -H 'Cache-Control: no-cache, no-store' -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.sh -o omniscience.sh
 
     	    chmod 700 sovereignty.sh
     	    chmod 700 backup.sh
@@ -244,7 +244,7 @@ else
     	    chmod 700 firewall.sh
     	    chmod 700 restore-backup.sh
     	    chmod 700 secure-os.sh
-    	    chmod 700 propylaxis.sh
+    	    chmod 700 omniscience.sh
 
     	    # check cron now!
     	    ./check-cron.sh
@@ -285,8 +285,8 @@ and run sovereignty.sh like normal\n\n\n"
     	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/firewall.sh -o firewall.sh
     	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/restore-backup.sh -o restore-backup.sh
     	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/linux-scripts/secure-os.sh -o secure-os.sh
-    	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.txt -o prophylaxis.txt
-    	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/prophylaxis.sh -o prophylaxis.sh
+    	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.txt -o omniscience.txt
+    	    curl -H 'Cache-Control: no-cache, no-store' -k -L https://raw.githubusercontent.com/CedarvilleCyber/CCDC/main/palo-alto/omniscience.sh -o omniscience.sh
 
 	        chmod 700 sovereignty.sh
 	        chmod 700 backup.sh
