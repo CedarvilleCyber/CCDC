95b997600a4da887f7b28c39eb03de30de571de5
kaichye <kaichengye11@gmail.com>
2025-02-28 21:30:07 -0500

    begin palo gen

---
+++ firewall/palo-gen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# 
+# palo-gen.sh
+# 
+# Generate palo commands based on input
+# 
+# Kaicheng Ye
+# Feb. 2025
+
+printf "${info}Starting palo-gen script${reset}\n"
+
+# zone names (find on web console after password change)
+# objects
+
+# priv & pub ips
+# service (port)
+
+# name of rule
+# source zone
+# source address
+# destination zone
+# destination address
+# application
+# service
+# action
+
+# write commands
+  # include profile, log-end, log-setting
+# output to file
+
+# end with commit, exit
+
+# run the finished file (ask for IP)
+
+printf "${info}Finished palo-gen script${reset}\n"
+
+exit 0
