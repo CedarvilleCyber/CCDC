4ce73339206c57817502545ffb80ae58e017773c
David Moore <113648012+David-B-Moore@users.noreply.github.com>
2026-03-12 16:44:28 -0400

    minor change for ecomm web creds

--- linux/ecomm-change-web-creds.php
+++ linux/ecomm-change-web-creds.php
@@ -1,14 +1,14 @@
 <?php
-// Usage: php ecomm-change-web-creds.php <web-username>
-[$_, $user] = $argv;
+// Usage: php ecomm-change-web-creds.php
 
-$db_user = readline("MySQL username: ");
-$db_pass = readline("MySQL password (input visible): ");
+require_once('/var/www/html/opencart/upload/config.php');
+
+$user = readline("Enter username to update: ");
 $pass = readline("New password for $user: ");
 $hash = password_hash($pass, PASSWORD_DEFAULT); # change to PASSWORD_BCRYPT if you have issues with the default algorithm
 
-$pdo = new PDO("mysql:host=localhost;dbname=opencart", $db_user, $db_pass);
-$stmt = $pdo->prepare('UPDATE oc_user SET password = ? WHERE username = ?');
+$pdo_temp = new PDO("mysql:host=localhost;dbname=opencart", DB_USERNAME, DB_PASSWORD);
+$stmt = $pdo_temp->prepare('UPDATE oc_user SET password = ? WHERE username = ?');
 $stmt->execute([$hash, $user]);
 echo "Updated password for user: $user\n";
 ?>
