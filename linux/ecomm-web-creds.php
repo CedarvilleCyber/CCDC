ce8f924e98ffad60b128b873235b2740d5fce58b
David <davidmoore155@cedarville.edu>
2026-03-12 21:11:50 -0400

    finished ecomm-web-creds.php

---
+++ linux/ecomm-web-creds.php
@@ -0,0 +1,41 @@
+<?php
+# Usage: php ecomm-change-web-creds.php
+# I wrote this in PHP to reduce breakable dependencies. OpenCart uses PHP and 
+# the mysqli driver, meaning this script SHOULD work on the CCDC ecomm machine 
+# without any changes. 
+
+# import OpenCart config to get DB credentials (remember to harden these)
+require_once('/var/www/html/opencart/upload/config.php');
+
+$version = readline("Enter OpenCart major version (3 or 4): ");
+$user = readline("Enter username to update: ");
+
+# eventually update this so passwords aren't visible
+$pass = readline("New password for $user: ");
+$pass_confirm = readline("Confirm password: ");
+
+if ($pass !== $pass_confirm) {
+    echo "Passwords do not match. Try again.\n";
+    exit(1);
+}
+
+$mysqli = new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if ($version == '3') {
+    $salt = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 9);
+    $hash = sha1($salt . sha1($salt . sha1($pass)));
+    $stmt = $mysqli->prepare('UPDATE oc_user SET password = ?, salt = ? WHERE username = ?');
+    $stmt->execute([$hash, $salt, $user]);
+}
+elseif ($version == '4') {
+    $hash = password_hash($pass, PASSWORD_DEFAULT); # change to PASSWORD_BCRYPT if you have issues with the default algorithm
+    $stmt = $mysqli->prepare('UPDATE oc_user SET password = ? WHERE username = ?');
+    $stmt->execute([$hash, $user]);
+}
+else {
+    echo "Invalid version. Please enter 3 or 4.\n";
+    exit(1);
+}
+
+echo "Updated password for user: $user\n";
+?>
